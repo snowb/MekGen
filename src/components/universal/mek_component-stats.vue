f7b0f8c51400d5f75cd9dc5214aecbe1caa156a4
snowb <ammoset@gmail.com>
2019-02-27 17:48:47 +0100

    creation of mek_component-stats slot component

---
+++ src/components/universal/mek_component-stats.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="outer-container">
+        <span class="mek-inline-flex-row subsection_container" style="text-align:left;">
+            <span class="padding-10px">
+                <slot name="col1-row1"></slot>
+                <slot name="col1-row2"></slot>
+                <slot name="col1-row3"></slot>
+            </span>
+            <span class="padding-10px">
+                <slot name="col2-row1"></slot>
+                <slot name="col2-row2"></slot>
+                <slot name="col2-row3"></slot>             
+            </span>
+            <span class="padding-10px">
+                <slot name="col3-row1"></slot>
+                <slot name="col3-row2"></slot>
+                <slot name="col3-row3"></slot>
+            </span>
+        </span>
+    </div>
+</template>
+<script>
+import utility_mixin from "../../../mixins/utility_mixin.js";
+
+export default
+{
+    name:"mek_magazine_stats",
+    mixins:[utility_mixin],
+    props:[],
+    data:function()
+    {
+        let obj={};
+        return obj;
+    },
+    methods:
+    {
+    },
+    computed:
+    {
+    }
+}
+</script>
+<style scoped>
+.padding-10px
+{
+    padding-right:10px; 
+    padding-left:10px;
+}
+.outer-container
+{
+    align-self:flex-start;
+    margin:5px;
+    font-size:120%;
+    color:#222;
+}
+
+</style>
