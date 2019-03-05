4edca5434744b012192ed9f20b77fce53e6d956c
snowb <ammoset@gmail.com>
2019-03-05 18:05:41 +0100

    recode for using universal comp_computed_mixin

--- src/components/mek_projectile/mek_projectile-main.vue
+++ src/components/mek_projectile/mek_projectile-main.vue
@@ -70,6 +70,7 @@
 <script>
 import selected_item_mixin from "../../mixins/selected_item_mixin";
 import utility_mixin from "../../mixins/utility_mixin";
+import component_computed_mixin from "../../mixins/component_computed_mixin";
 
 /* import mek_projectile_damage from "./subcomponents/mek_projectile-damage.vue";
 import mek_projectile_accuracy from "./subcomponents/mek_projectile-accuracy.vue";
@@ -88,7 +89,7 @@ export default
 {
     name:"mek_projectile",
     props:[],
-    mixins:[selected_item_mixin, utility_mixin],
+    mixins:[selected_item_mixin, utility_mixin, component_computed_mixin],
     components:
     {
         "mek-projectile-damage":()=>import("./subcomponents/mek_projectile-damage.vue"),
@@ -109,8 +110,8 @@ export default
         let obj={};
         obj.uuid=null;
         obj.component_name=null;
-        obj.component_category=null;
-        obj.component_type=null;
+        obj.component_category="equipment";
+        obj.component_type="projectile";
         obj.original_component=null;
         obj.component_changed=true;
 
@@ -269,45 +270,12 @@ export default
         {
             return this.round(this.selected_damage.cost * this.cost_multiplier,2);
         },
-        space_cost:function()
-        {
-            return this.raw_space - this.efficiencies.space.modifier;
-        },
-        cost_multiplier()
-        {
-            let cost_multiplier=1;
-            for(let multi in this.cost_multipliers)
-            {
-                cost_multiplier*=this.cost_multipliers[multi];
-            }
-            return this.round(cost_multiplier,2);
-        },
         cost:function()
         {
             let subtotal_cost=this.selected_damage.cost * this.cost_multiplier;
             subtotal_cost += this.efficiencies.space.cost;
 
             return this.round(subtotal_cost,2);
-        },
-        weight:function()
-        {
-            return this.round((this.damage_capacity / 2),2);
-        },
-        newComponent()
-        {
-            let selectedComponent=JSON.parse(JSON.stringify(this.$store.getters.selectedComponent));
-            
-            if(typeof selectedComponent!=="undefined" && selectedComponent!==null)
-            {
-                if(selectedComponent.uuid!==this.uuid 
-                    && selectedComponent.component_category=="equipment" 
-                    && selectedComponent.component_type=="projectile")
-                {
-                    this.ingest_data(selectedComponent);
-                }
-                return false;
-            }
-            return true;
         },
         projectile_name()
         {
@@ -322,15 +290,6 @@ export default
             projectile_name=projectile_name.length>0?projectile_name+" Gun":"Gun";
 
             return projectile_name.replace(/\s+/g," ");
-        },
-        feature_list()
-        {
-            return this.feature_array.reduce(function(_string, _val, _index)
-            {
-                _string+=_index>0 ? ", " : "";
-                _string+=_val.feature;
-                return _string;
-            },"");
         }
     }
 };
