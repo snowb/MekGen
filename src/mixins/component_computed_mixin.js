64b99af485a44d9137df504c080f7c8cc1eb53ed
snowb <ammoset@gmail.com>
2019-03-05 18:04:49 +0100

    add feature_list and newComp

--- src/mixins/component_computed_mixin.js
+++ src/mixins/component_computed_mixin.js
@@ -19,5 +19,34 @@ export default
         {
             return this.round((this.damage_capacity / 2),2);
         },
+        feature_list()
+        {
+            if(typeof this.feature_array==="undefined")
+            {
+                return "";
+            }
+            return this.feature_array.reduce(function(_string, _val, _index)
+            {
+                _string+=_index>0 ? ", " : "";
+                _string+=_val.feature;
+                return _string;
+            },"");
+        },
+        newComponent()
+        {
+            let selectedComponent=JSON.parse(JSON.stringify(this.$store.getters.selectedComponent));
+            
+            if(typeof selectedComponent!=="undefined" && selectedComponent!==null)
+            {
+                if(selectedComponent.uuid!==this.uuid 
+                    && selectedComponent.component_category==this.component_category
+                    && selectedComponent.component_type==this.component_type)
+                {
+                    this.ingest_data(selectedComponent);
+                }
+                return false;
+            }
+            return true;
+        },
     }
 };
