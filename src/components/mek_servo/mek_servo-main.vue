a9437de5ec3a9aa9103f1e9fb4cab5dbd720cd73
snowb <ammoset@gmail.com>
2019-03-05 20:36:32 +0100

    added servo-type subcomp

--- src/components/mek_servo/mek_servo-main.vue
+++ src/components/mek_servo/mek_servo-main.vue
@@ -1,28 +1,58 @@
 <template>
     <span class="mek-inline-flex-col" style="width:100%;">
-        <div style="align-self:flex-start;">
-        servo
-        </div>
+        <mek-servo-type :servo-type="selected_servo_type" @update-servo-type="updateServoType"
+        ></mek-servo-type>
     </span>
 </template>
 
 <script>
-import servo_classes_mixin from "../../mixins/servo_classes_mixin";
-import selected_item_mixin from "../../mixins/selected_item_mixin";
-import utility_mixin from "../../mixins/utility_mixin";
+import servo_classes_mixin from "../../mixins/servo_classes_mixin.js";
+import selected_item_mixin from "../../mixins/selected_item_mixin.js";
+import utility_mixin from "../../mixins/utility_mixin.js";
+import component_methods_mixin from "../../mixins/component_methods_mixin";
+import component_computed_mixin from "../../mixins/component_computed_mixin";
 
 export default
 {
     name:"mek_servo",
     props:[],
-    mixins:[servo_classes_mixin, selected_item_mixin, utility_mixin],
+    mixins:
+        [
+            servo_classes_mixin, 
+            selected_item_mixin, 
+            utility_mixin, 
+            component_methods_mixin, 
+            component_computed_mixin
+        ],
+    components:
+    {
+        "mek-servo-type":()=>import("./subcomponents/mek_servo-type.vue"),
+
+        "mek-armor-type":()=>import("../universal/mek_armor-type.vue"),
+        "mek-energy-absorbing-armor":()=>import("../universal/mek_energy-absorbing-armor.vue")
+    },
     data:function()
     {
-        return {};
+        let obj={};
+        obj.uuid=null;
+        obj.type="standard";
+        obj.component_name=null;
+        obj.component_category="servo";
+        obj.component_type="";
+        obj.original_component=null;
+        obj.component_changed=true;
+
+        obj.selected_servo_type={type:"Torso"};
+
+        return obj;
     },
     methods:
     {
-
+        updateServoType(_servo_type)
+        {   
+            this.component_type=_servo_type.type;
+            this.$set(this,"selected_servo_type",_servo_type)
+        }
     }
 };
 </script>
