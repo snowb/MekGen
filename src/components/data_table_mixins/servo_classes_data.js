7a6e23f2defbe9b38ea093dcbe33c378a778bf88
snowb <ammoset@gmail.com>
2019-03-10 16:55:02 +0100

    creation of standalone servo_classes data

---
+++ src/components/data_table_mixins/servo_classes_data.js
@@ -0,0 +1,15 @@
+export default
+    [
+        {code:1,id:"SL",name:"Superlight"},
+        {code:2,id:"LW",name:"Lightweight"},
+        {code:3,id:"STR",name:"Striker"},
+        {code:4,id:"MS",name:"Medium Striker"},
+        {code:5,id:"HS",name:"Heavy Striker"},
+        {code:6,id:"MW",name:"Mediumweight"},
+        {code:7,id:"LH",name:"Light Heavy"},
+        {code:8,id:"MH",name:"Medium Heavy"},
+        {code:9,id:"AH",name:"Armored Heavy"},
+        {code:10,id:"SH",name:"Super Heavy"},
+        {code:11,id:"MgH",name:"Mega Heavy"}
+    ];
+
