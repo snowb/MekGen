8296dedc0280bcfae77defa595268cc0601cce5c
snowb <ammoset@gmail.com>
2019-03-14 20:09:28 +0100

    cental prune feat method, toggle feat method

--- src/components/mek_magazine/subcomponents/mek_magazine-ammo-list.vue
+++ src/components/mek_magazine/subcomponents/mek_magazine-ammo-list.vue
@@ -10,9 +10,10 @@
 import selected_item_mixin from "../../../mixins/selected_item_mixin";
 import utility_mixin from "../../../mixins/utility_mixin";
 
-import {ammo_data_table, ammo_validate, shock_exclusive, blast_exclusive} from "../../data_table_modules/mek_ammo-list-data-module.js";
+import {ammo_data_table, ammo_validate, has_feature, get_feature, shock_exclusive, blast_exclusive} from "../../data_table_modules/mek_ammo-list-data-module.js";
 
 import mek_sub_component_table from "../../universal/mek_sub-component-table.vue";
+import { constants } from 'fs';
 export default
 {
     name:"mek_magazine_ammo_list",
@@ -32,64 +33,103 @@ export default
     methods:
     {
         select_ammo:function(_selected_ammo)
-        {//refactor to pkey selector
-
-            let selected_pkey=_selected_ammo[this.pkey];
-            let isExclusiveShock=this.is_exclusive_feature("shock_exclusive",selected_pkey);
-            let isExclusiveBlast=this.is_exclusive_feature("blast_exclusive",selected_pkey);
-            let featureClone=JSON.parse(JSON.stringify(_selected_ammo));
-
-            let temp_selected_feature_array=this.selected_ammo_array.filter((_val)=>
-            {//filter out matching feature (toggle)
-                return _val[this.pkey].toLowerCase()!=selected_pkey.toLowerCase();
-            },this);
-
-            let togglefeature=this.selected_ammo_array.some((_elem)=>
-            {
-                return _elem[this.pkey].toLowerCase()==selected_pkey.toLowerCase();
-            },this);
-
-            if(isExclusiveShock)
-            {//filter out exclusive phalanx
-                temp_selected_feature_array=temp_selected_feature_array.filter((_val)=>
-                {
-                    return !_val.shock_exclusive;
-                });
-            }
-            if(isExclusiveBlast)
-            {//filter out exclusive phalanx
-                temp_selected_feature_array=temp_selected_feature_array.filter((_val)=>
-                {
-                    return !_val.blast_exclusive;
-                });
-            }
-
-            if(!togglefeature)
-            {
-                temp_selected_feature_array.push(featureClone);
-            }
-            
-            this.$set(this,"selected_ammo_array",JSON.parse(JSON.stringify(temp_selected_feature_array)));
-            
-            if(temp_selected_feature_array.length==0)
-            {
-                this.$set(this,"selected_ammo_array",[]);
-            }
-
-            this.$emit("update-ammo",JSON.parse(JSON.stringify(this.selected_ammo_array)));
+        {//refactor to external ammo array cleaner
+            let new_selected_ammo_array=this.toggleFeature(this.selected_ammo_array,_selected_ammo);
+            new_selected_ammo_array=this.cleanFeatureArray(new_selected_ammo_array).cleaned_array;
+            this.$set(this,"selected_ammo_array",new_selected_ammo_array);
+            this.$emit("update-ammo",new_selected_ammo_array);
         },
-        get_feature(_pkey_value)
-        {
-            let found_feature=null;
-            ammo_data_table.some((_val)=>
+        cleanFeatureArray(_feature_array)
+        {//takes feature_array, returns cleaned array removing multiple exclusive values
+            let hasExclusiveShock=false;
+            let hasExclusiveBlast=false;
+            let self=this;
+            let update=false;
+            let key_list=[];
+
+            let temp_selected_feature_array=_feature_array.reduceRight((_cleaned_array, _val)=>
             {
-                if(_val[this.pkey]==_pkey_value)
-                {
-                    found_feature=_val;
-                    return true;
+                if(!has_feature(self.pkey,_val[self.pkey]))
+                {//if feature with pkey doesn't exist in data table, ignore
+                    return _cleaned_array;
                 }
+                let clean_feature=_val;
+                if(!ammo_validate(_val))
+                {//invalid data
+                    clean_feature=get_feature(_val[self.pkey]);
+                    update=true;
+                    //attempt to set to corrected feature
+                }
+                if(clean_feature===null)
+                {//no matching feature
+                    update=true;
+                    return _cleaned_array;
+                    //ignore element
+                }
+                let isShock=self.is_exclusive_feature("shock_exclusive",_val[self.pkey]);
+                let isBlast=self.is_exclusive_feature("blast_exclusive",_val[self.pkey]);
+
+                if(isShock && !hasExclusiveShock)
+                {
+                    _cleaned_array.push(_val);
+                    hasExclusiveShock=true;
+                    key_list.push(_val[self.pkey]);
+                    return _cleaned_array;
+                }
+                else if(isShock && hasExclusiveShock)
+                {
+                    update=true;
+                    return _cleaned_array;
+                }
+
+                if(isBlast && !hasExclusiveBlast)
+                {
+                    _cleaned_array.push(_val);
+                    hasExclusiveBlast=true;
+                    key_list.push(_val[self.pkey]);
+                    return _cleaned_array;
+                }
+                else if(isBlast && hasExclusiveBlast)
+                {
+                    update=true;
+                    return _cleaned_array;
+                }
+
+                if(!key_list.includes(_val[self.pkey]))
+                {
+                    _cleaned_array.push(_val);
+                    key_list.push(_val[self.pkey]);
+                }
+                return _cleaned_array;
+            },[]);
+            temp_selected_feature_array=temp_selected_feature_array.filter((_val)=>
+            {
+                return _val[this.pkey].toLowerCase()!="nuclear" 
+                    || (hasExclusiveBlast && _val[this.pkey].toLowerCase()=="nuclear");
+                //filter out nuclear if blast-radius isn't a feature
+            },this).reverse();
+            return {cleaned_array:temp_selected_feature_array,update:update,key_list:key_list};
+            //returns an object with the pruned feature array, whether it was updated, and the key_list
+        },
+        toggleFeature(_feature_array,_feature)
+        {
+            let feature_array=JSON.parse(JSON.stringify(_feature_array));
+            
+            let remove_feature=feature_array.some((_val)=>
+            {//if _feature matches already existing feature, flag for deletion
+                return _val[this.pkey]==_feature[this.pkey];
             },this);
-            return found_feature;
+
+            if(remove_feature)
+            {//if flagged for removal, filter out
+                return feature_array.filter((_val)=>
+                {
+                    return _val[this.pkey]!=_feature[this.pkey];
+                },this);
+            }
+            feature_array.push(_feature);
+            //otherwise add feature and return
+            return feature_array;
         },
         is_exclusive_feature:function(_target_array,_feature)
         {
@@ -122,14 +162,14 @@ export default
         {
             if(this.ammoArray.length==0)
             {
-                let feature_clone=JSON.parse(JSON.stringify(this.get_feature("High-Ex")));
+                let feature_clone=JSON.parse(JSON.stringify(get_feature("High-Ex")));
                 this.$set(this,"selected_ammo_array",[feature_clone]);
                 return ["High-Ex"];
             }
             if(this.ammoArray.length==1)
             {
                 let pkey_value=this.ammoArray[0][this.pkey];
-                let feature_clone=JSON.parse(JSON.stringify(this.get_feature(pkey_value)));
+                let feature_clone=JSON.parse(JSON.stringify(get_feature(pkey_value)));
 
                 if(!ammo_validate(this.ammoArray[0]))
                 {
@@ -139,47 +179,14 @@ export default
                 return [pkey_value];
             }
 
-            let key_list=[];
-            let hasExclusiveShock=false;
-            let hasExclusiveBlast=false;
-            let self=this;
-            let update_data=false;
+            let cleaned_array=this.cleanFeatureArray(this.ammoArray);
 
-            let temp_selected_feature_array=this.ammoArray.reduceRight((_cleaned_array, _val)=>
+            if(cleaned_array.update)
             {
-                let isShock=self.is_exclusive_feature("shock_exclusive",_val[self.pkey]);
-                let isBlast=self.is_exclusive_feature("blast_exclusive",_val[self.pkey]);
-
-                if(isShock && !hasExclusiveShock)
-                {
-                    _cleaned_array.push(_val);
-                    hasExclusiveShock=true;
-                    key_list.push(_val[self.pkey]);
-                }
-                if(isBlast && !hasExclusiveBlast)
-                {
-                    _cleaned_array.push(_val);
-                    hasExclusiveBlast=true;
-                    key_list.push(_val[self.pkey]);
-                }
-
-                if(!key_list.includes(_val[self.pkey]))
-                {
-                    _cleaned_array.push(_val);
-                    key_list.push(_val[self.pkey]);
-                }
-                update_data=!ammo_validate(_val) && update_data===null ? true : update_data;
-                //if invalid and update_data not set, set data
-
-                return _cleaned_array;
-            },[]);
-
-            if(update_data)
-            {
-                this.$emit("update-ammo",JSON.parse(JSON.stringify(temp_selected_feature_array)));
+                this.$emit("update-ammo",cleaned_array.cleaned_array);
             }
 
-            return key_list;
+            return cleaned_array.key_list;
         }
     }
 }
