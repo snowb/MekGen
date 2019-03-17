47787cc9b41a83245996a97e068687fe776b8d35
snowb <ammoset@gmail.com>
2019-03-17 14:29:25 +0100

    move data to module, refac to pkey selector

--- src/components/mek_servo/subcomponents/mek_servo-type.vue
+++ src/components/mek_servo/subcomponents/mek_servo-type.vue
@@ -1,14 +1,14 @@
 <template>
     <mek-sub-component-table
-        :items="type_table" :headers="{type:''}"
-        name="Type" flow="col" :showHeaders="false"
-        :selectedIndices="type_index"
-        @update-selected-indices="select_type"
+        :items="type_table" :headers="{type:''}" :pkey="pkey" :selectedKeys="selected_keys"
+        name="Type" flow="pkey-col" :showHeaders="false"
+        @update-selected-data="select_type"
     ></mek-sub-component-table>
 </template>
 <script>
 import selected_item_mixin from "../../../mixins/selected_item_mixin.js";
 import utility_mixin from "../../../mixins/utility_mixin.js";
+import {servo_data_table, servo_type_validate, has_feature, get_feature} from "../../data_table_modules/mek_servo-type-data-module.js";
 
 export default 
 {
@@ -22,24 +22,14 @@ export default
     data:()=>
     {
         let obj={};
-        obj.type_table=
-        [
-            {type:"Torso"},
-            {type:"Head"},
-            {type:"Arm"},
-            {type:"Leg"},
-            {type:"Wing"},
-            {type:"Tail"},
-            {type:"Pod"},
-        ];
-
+        obj.pkey="type";
         return obj;
     },
     methods:
     {
-        select_type:function(_selected_type_index)
+        select_type:function(_selected_type)
         {
-            this.$emit("update-servo-type",this.type_table[_selected_type_index]);
+            this.$emit("update-servo-type",JSON.parse(JSON.stringify(_selected_type)));
         }
     },
     computed:
@@ -57,6 +47,36 @@ export default
             },this);
 
             return [index];
+        },
+        selected_keys()
+        {
+            let default_data=get_feature(this.pkey,"Torso");
+            if(this.servoType===undefined)
+            {
+                this.select_type(default_data);
+            }
+
+            let json_data=JSON.stringify(this.servoType);
+            if(!has_feature(this.pkey,this.servoType[this.pkey]))
+            {
+                this.addAlert("Mek_Servo-Type: "+json_data);
+                this.addAlert("**** Invalid data. Reseting to default. ****");
+                this.publishAlerts();
+                this.select_type(default_data);
+                return [default_data[this.pkey]];
+            }
+            else if(!servo_type_validate(this.servoType))
+            {
+                this.addAlert("Mek_Servo-Type: "+json_data);
+                this.addAlert("**** Invalid data. Reseting. ****");
+                this.publishAlerts();
+                this.select_type(get_feature(this.pkey,this.servoType[this.pkey]));
+            }
+            return [this.servoType[this.pkey]];
+        },
+        type_table()
+        {
+            return servo_data_table;
         }
     }
 }
