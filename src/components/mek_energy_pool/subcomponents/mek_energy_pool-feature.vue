b1d350b5a89fdde6aea4ad90e2b40c7296102332
snowb <ammoset@gmail.com>
2019-03-17 21:46:57 +0100

    switch to pkey selector, added alerts

--- src/components/mek_energy_pool/subcomponents/mek_energy_pool-feature.vue
+++ src/components/mek_energy_pool/subcomponents/mek_energy_pool-feature.vue
@@ -1,24 +1,23 @@
 <template>
     <mek-sub-component-table
-        :items="feature_table"
+        :items="feature_table" :pkey="pkey" :selectedKeys="selected_keys"
         :headers="{feature:'Feature',cost:'Cost'}"
         :showHeaders="true" :format="{cost:'multiplier'}"
-        :selected-indices="selected_feature_index_array"
-        @update-selected-indices="select_feature"
-        name="Features"
-        flow="col"
+        @update-selected-data="select_feature"
+        name="Features" flow="pkey-col"
     ></mek-sub-component-table>
 </template>
 <script>
 import selected_item_mixin from "../../../mixins/selected_item_mixin";
 import utility_mixin from "../../../mixins/utility_mixin";
+import alerts_mixin from "../../../mixins/alerts_mixin";
 
 import mek_sub_component_table from "../../universal/mek_sub-component-table.vue";
 export default 
 {
     name:"mek_energy_pool_feature",
     props:["featureArray"],
-    mixins:[selected_item_mixin, utility_mixin],
+    mixins:[selected_item_mixin, utility_mixin, alerts_mixin],
     components:
     {
         "mek-sub-component-table":mek_sub_component_table
@@ -33,114 +32,160 @@ export default
             ];
 
         obj.selected_feature_array=[];
+        obj.pkey="feature";
+        obj.alerts=[];
         return obj;
     },
     methods:
     {
-        select_feature:function(_selected_feature_index)
+        select_feature:function(_selected_feature)
         {
-            let select_feature_name=this.feature_table[_selected_feature_index].feature;
-            let featureClone=Object.assign({},this.feature_table[_selected_feature_index]);
-
-            let temp_selected_feature_array=this.selected_feature_array.filter((_val)=>
-            {//filter out matching feature (toggle)
-                return _val.feature.toLowerCase()!=select_feature_name.toLowerCase();
-            });
-
-            let togglefeature=this.selected_feature_array.some((_elem)=>
-            {
-                return _elem.feature.toLowerCase()==select_feature_name.toLowerCase();
-            },this);
-
-            if(!togglefeature)
-            {
-                temp_selected_feature_array.push(featureClone);
-            }
-            
-            this.$set(this,"selected_feature_array",temp_selected_feature_array);
-            
-            if(temp_selected_feature_array.length==0)
-            {
-                this.$set(this,"selected_feature_array",[]);
-            }
-
-            this.$emit("update-feature",this.selected_feature_array);
+            let new_selected_feature_array=this.toggleFeature(this.selected_feature_array,_selected_feature);
+            new_selected_feature_array=this.cleanFeatureArray(new_selected_feature_array).cleaned_array;
+            this.publishAlerts();
+            this.$set(this,"selected_feature_array",new_selected_feature_array);
+            this.$emit("update-feature",new_selected_feature_array);
         },
-        find_feature_index:function(_feature)
+        feature_validate(_data)
         {
-            let found_index;
-            this.feature_table.some(function(_val, _index)
+            if(typeof _data==="undefined")
             {
-                if(_val.feature.toLowerCase() == _feature.toLowerCase())
-                {
-                    found_index=_index;
-                    return true;
-                }
                 return false;
+            }
+            let valid=this.feature_table.some((_val)=>
+            {
+                return _val.feature==_data.feature
+                        && _val.cost==_data.cost;
             });
+            return valid;
+        },
+        has_feature(_key, _val)
+        {
+            return this.feature_table.some((_data)=>
+            {
+                return _data[_key]!==undefined && _data[_key]==_val;
+            });
+        },
+        get_feature(_key, _val)
+        {
+            if(this.has_feature(_key,_val))
+            {
+                let found_feature=null;
+                this.feature_table.some((_table_val)=>
+                {
+                    if(_table_val[_key]==_val)
+                    {
+                        found_feature=_table_val;
+                        return true;
+                    }
+                },this);
+                return found_feature;
+            }
+        },
+        cleanFeatureArray(_feature_array)
+        {//takes feature_array, returns cleaned array removing multiple exclusive values
+            let self=this;
+            let update=false;
+            let key_list=[];
 
-            return found_index;
+            let temp_selected_feature_array=_feature_array.reduceRight((_cleaned_array, _val)=>
+            {
+                if(_val[self.pkey]===undefined)
+                {//if feature with pkey doesn't exist in data table, ignore
+                    self.addAlert("Mek_Energy_Pool-Feature: "+JSON.stringify(_val));
+                    self.addAlert("**** Missing primary key. Ignoring. ****");
+                    return _cleaned_array;
+                }
+                let clean_feature=_val;
+                if(!this.feature_validate(_val))
+                {//invalid data
+                    self.addAlert("Mek_Energy_Pool-Feature: "+JSON.stringify(_val))
+                    self.addAlert("**** Invalid data, attempting to reset. ****")
+                    clean_feature=this.get_feature(self.pkey,_val[self.pkey]);
+                    update=true;
+                    //attempt to set to corrected feature
+                }
+                if(clean_feature===null)
+                {//no matching feature
+                    self.addAlert("**** Unable to reset. No matching data. ****")
+                    update=true;
+                    return _cleaned_array;
+                    //ignore element
+                }
+                if(!key_list.includes(_val[self.pkey]))
+                {
+                    _cleaned_array.push(_val);
+                    key_list.push(_val[self.pkey]);
+                }
+                return _cleaned_array;
+            },[]);
+            temp_selected_feature_array=temp_selected_feature_array.reverse();
+            return {cleaned_array:temp_selected_feature_array,update:update,key_list:key_list};
+            //returns an object with the pruned feature array, whether it was updated, and the key_list
+        },
+        toggleFeature(_feature_array,_feature)
+        {
+            let feature_array=JSON.parse(JSON.stringify(_feature_array));
+            
+            let remove_feature=feature_array.some((_val)=>
+            {//if _feature matches already existing feature, flag for deletion
+                return _val[this.pkey]==_feature[this.pkey];
+            },this);
+            if(remove_feature)
+            {//if flagged for removal, filter out
+                return feature_array.filter((_val)=>
+                {
+                    return _val[this.pkey]!=_feature[this.pkey];
+                },this);
+            }
+            feature_array.push(_feature);
+            //otherwise add feature and return
+            return feature_array;
         },
     },
     computed:
     {
-        selected_feature_index_array:function()
+        selected_keys()
         {
-            let indices=[];
             if(this.featureArray.length==0)
             {
-                this.selected_feature_array=[];
+                return [];
             }
             if(this.featureArray.length==1)
             {
-                this.selected_feature_array=[this.feature_table[this.find_feature_index(this.featureArray[0].feature)]];
-                return [this.find_feature_index(this.featureArray[0].feature)];
-            }
-
-            let self=this;
-            let feature_list=[];
-
-            this.selected_feature_array=this.featureArray.reduceRight(function(_prev, _val)
-            {
-                if(!feature_list.includes(_val.feature.toLowerCase()))
+                let pkey_value=this.featureArray[0][this.pkey];
+                if(pkey_value===undefined || !this.has_feature(this.pkey,pkey_value))
                 {
-                    _prev.push(_val);
-                    feature_list.push(_val.feature.toLowerCase());
+                    let json_data=JSON.stringify(this.featureArray[0]);
+                    this.addAlert("Mek_Energy_Pool-Feature: "+json_data);
+                    this.addAlert("**** Invalid data. Reseting to default. ****");
+                    this.publishAlerts();
+                    this.select_ammo([]);
+                    return [];
                 }
-                return _prev;
-            },[]);
-
-            indices=this.selected_feature_array.reduce(function(_indices,_val)
-            {
-                _indices.push(self.find_feature_index(_val.feature));
-                return _indices;
-            },[]);
-            if(this.featureArray.length!=this.selected_feature_array.length)
-            {
-                this.$emit("update-feature",this.selected_feature_array);
-            }
-
-            return indices;
-        },
-        /* flat_feature_array:function()
-        {
-            return this.feature_table.reduce(function(_array, _el)
-            {
-                _array.push(_el.feature);
-                return _array;
-            },[]);
-        }, */
-        /* filteredFeatureTable()
-        {
-            if(this.burstValue<=1)
-            {
-                return this.feature_table.filter((_val)=>
+                else if(this.has_feature(this.pkey,pkey_value) && !this.feature_validate(this.featureArray[0]))
                 {
-                    return !this.is_exclusive_feature("exclusive_anti_missile",_val.feature);// || this.is_exclusive_feature("exclusive_personnel",_val.feature) ;
-                },this);
+                    let json_data=JSON.stringify(this.get_feature(this.pkey,pkey_value));
+                    let feature_clone=JSON.parse(json_data);
+                    this.addAlert("Mek_Energy_Pool-Feature: "+json_data);
+                    this.addAlert("**** Invalid data. Reseting. ****");
+                    this.publishAlerts();
+                    this.select_ammo(feature_clone);
+                }
+                
+                return [pkey_value];
             }
-             return this.feature_table;
-        }*/
+
+            let cleaned_array=this.cleanFeatureArray(this.featureArray);
+
+            if(cleaned_array.update)
+            {c
+                this.$set(this,"feature_table",cleaned_array.cleaned_array);
+                this.$emit("update-feature",cleaned_array.cleaned_array);
+            }
+
+            return cleaned_array.key_list;
+        }
     },
 }
 </script>
