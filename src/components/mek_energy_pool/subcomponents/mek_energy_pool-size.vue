87a14d78365f82163b5a82fcbca89d8e706e9b0e
snowb <ammoset@gmail.com>
2019-03-18 12:55:52 +0100

    switch to pkey-selector, add alerts, data module

--- src/components/mek_energy_pool/subcomponents/mek_energy_pool-size.vue
+++ src/components/mek_energy_pool/subcomponents/mek_energy_pool-size.vue
@@ -1,22 +1,25 @@
 <template>
     <mek-sub-component-table
-        :items="portfolio_size_table"
+        :items="portfolio_size_table" :selectedKeys="selected_keys" :pkey="pkey"
         :headers="{size:'Portfolio Size',cost:'Cost'}"
-        name="Portfolio Size" flow="row" :showHeaders="true"
-        :selectedIndices="portfolio_size_index"
-        @update-selected-indices="select_portfolio_size"
+        name="Portfolio Size" flow="pkey-row" :showHeaders="true"
+        @update-selected-data="select_portfolio_size"
     ></mek-sub-component-table>
 </template>
 <script>
 import selected_item_mixin from "../../../mixins/selected_item_mixin";
 import utility_mixin from "../../../mixins/utility_mixin";
+import alerts_mixin from "../../../mixins/alerts_mixin";
+
+import {energy_pool_size_data_table, energy_pool_size_validate, has_feature, get_feature}
+    from "../../data_table_modules/mek_energy_pool-size-data-module";
 
 import mek_sub_component_table from "../../universal/mek_sub-component-table.vue";
 export default
 {
     name:"mek_portfolio_size",
     props:["portfolioSize"],
-    mixins:[selected_item_mixin,utility_mixin],
+    mixins:[selected_item_mixin,utility_mixin,alerts_mixin],
     components:
     {
         "mek-sub-component-table":mek_sub_component_table
@@ -24,57 +27,56 @@ export default
     data:function()
     {
         let obj={};
-
-        obj.portfolio_size_table=
-        [
-            {size:1,cost:0.6},
-            {size:2,cost:0.75},
-            {size:3,cost:1},
-            {size:4,cost:1.2},
-            {size:5,cost:1.3},
-            {size:7,cost:1.4},
-            {size:10,cost:1.5},
-            {size:Infinity,cost:2},
-        ];
-
         obj.selected_portfolio_size={damage:1,cost:1.5,range:4};
+        obj.alerts=[];
+        obj.pkey="cost";
 
         return obj;
     },
     methods:
     {
-        select_portfolio_size:function(_portfolio_size_index)
+        select_portfolio_size:function(_portfolio_size)
         {
-            this.$emit("update-portfolio-size", this.portfolio_size_table[_portfolio_size_index]);
+            let data=JSON.parse(JSON.stringify(_portfolio_size));
+            let infinite_size=_portfolio_size.size===Infinity;
+            if(infinite_size)
+            {
+                data.size=Infinity;
+            }
+            this.$emit("update-portfolio-size", data);
         },
     },
     computed:
     {
-        portfolio_size_index()
+        portfolio_size_table()
         {
-            let portfolio_size_index=0;
-            this.portfolio_size_table.some((_val,_index)=>
-            {
-                if(this.portfolioSize.cost==_val.cost)
-                {
-                    portfolio_size_index=_index;
-                    return true;
-                }
-            });
+            return energy_pool_size_data_table;
+        },
+        selected_keys()
+        {
+            let default_data=get_feature(this.pkey,1);
 
-            let update=false;
-            switch(true)
+            if(this.portfolioSize===undefined)
             {
-                case this.portfolioSize.power_available!=this.portfolio_size_table[portfolio_size_index].power_available:
-                case this.portfolioSize.max_power!=this.portfolio_size_table[portfolio_size_index].max_power:
-                case this.portfolioSize.damage_capacity!=this.portfolio_size_table[portfolio_size_index].damage_capacity:
-                    update=true;       
+                this.select_portfolio_size(default_data);
             }
-            if(update)
-            {   
-                this.select_portfolio_size(portfolio_size_index);
+            let json_data=JSON.stringify(this.portfolioSize);
+            if(!has_feature(this.pkey,this.portfolioSize[this.pkey]))
+            {
+                this.addAlert("Mek_Energy_Pool-Size: "+json_data);
+                this.addAlert("**** Invalid data. Reseting to default. ****");
+                this.publishAlerts();
+                this.select_portfolio_size(default_data);
+                return [default_data[this.pkey]];
             }
-            return [portfolio_size_index];
+            else if(!energy_pool_size_validate(this.portfolioSize))
+            {
+                this.addAlert("Mek_Energy_Pool-Size: "+json_data);
+                this.addAlert("**** Invalid data. Reseting. ****");
+                this.publishAlerts();
+                this.select_portfolio_size(get_feature(this.pkey,this.portfolioSize[this.pkey]));
+            }
+            return [this.portfolioSize[this.pkey]];
         }
     }
 }
