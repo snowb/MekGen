4762836875cbbf4c9c092151d257863a02499438
snowb <ammoset@gmail.com>
2019-03-19 11:10:02 +0100

    switch to pkey selector, alerts

--- src/components/mek_melee/subcomponents/mek_melee-damage.vue
+++ src/components/mek_melee/subcomponents/mek_melee-damage.vue
@@ -1,22 +1,25 @@
 <template>
     <mek-sub-component-table
-        :items="damage_table"
+        :items="damage_table" :selectedKeys="selected_keys" :pkey="pkey"
         :headers="{damage:'Damage',cost:'Cost'}"
-        name="Damage" flow="row" :showHeaders="true"
-        :selectedIndices="damage_index"
-        @update-selected-indices="select_damage"
+        name="Damage" flow="pkey-row" :showHeaders="true"
+        @update-selected-data="select_damage"
     ></mek-sub-component-table>
 </template>
 <script>
 import selected_item_mixin from "../../../mixins/selected_item_mixin";
 import utility_mixin from "../../../mixins/utility_mixin";
+import alerts_mixin from "../../../mixins/alerts_mixin";
+
+import {damage_data_table, damage_validate, has_feature, get_feature}
+    from "../../data_table_modules/mek_melee/mek_melee-damage-data-module.js";
 
 import mek_sub_component_table from "../../universal/mek_sub-component-table.vue";
 export default
 {
     name:"mek_melee_damage",
     props:["damage"],
-    mixins:[selected_item_mixin,utility_mixin],
+    mixins:[selected_item_mixin,utility_mixin,alerts_mixin],
     components:
     {
         "mek-sub-component-table":mek_sub_component_table
@@ -24,53 +27,49 @@ export default
     data:function()
     {
         let obj={};
-
-        obj.damage_table=Array.apply({}, Array(20));
-        
-         obj.damage_table.forEach((_el,_index)=>
-        {
-            let damage=_index+1;
-            let cost=damage/2;
-            obj.damage_table[_index]={damage:damage,cost:cost};
-        },this);
-
-        obj.selected_damage={damage:1,cost:0.5};
-
+        obj.alerts=[];
+        obj.pkey="damage";
         return obj;
     },
     methods:
     {
         select_damage:function(_damage)
         {
-            this.$emit("update-damage",this.damage_table[_damage]);
+            let data=JSON.parse(JSON.stringify(_damage));
+            this.$emit("update-damage",data);
         },
     },
     computed:
     {
-        damage_index()
+        damage_table()
         {
-            let damage_index=0;
-            this.damage_table.some((_val,_index)=>
-            {
-                if(this.damage.damage==_val.damage)
-                {
-                    damage_index=_index;
-                    return true;
-                }
-            });
+            return damage_data_table;
+        },
+        selected_keys()
+        {
+            let default_data=get_feature(this.pkey,0);
 
-            let update=false;
-            switch(true)
+            if(this.damage===undefined)
             {
-                case this.damage.damage!=this.damage_table[damage_index].damage:
-                case this.damage.cost!=this.damage_table[damage_index].cost:
-                    update=true;       
+                this.select_damage(default_data);
             }
-            if(update)
-            {   
-                this.select_damage(damage_index);
+            let json_data=JSON.stringify(this.damage);
+            if(!has_feature(this.pkey,this.damage[this.pkey]))
+            {
+                this.addAlert("Mek_Melee-Damage: "+json_data);
+                this.addAlert("**** Invalid data. Reseting to default. ****");
+                this.publishAlerts();
+                this.select_damage(default_data);
+                return [default_data[this.pkey]];
             }
-            return [damage_index];
+            else if(!damage_validate(this.damage))
+            {
+                this.addAlert("Mek_Melee-Damage: "+json_data);
+                this.addAlert("**** Invalid data. Reseting. ****");
+                this.publishAlerts();
+                this.select_damage(get_feature(this.pkey,this.damage[this.pkey]));
+            }
+            return [this.damage[this.pkey]];
         }
     }
 }
