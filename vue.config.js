63a74ec066114c8095561cf67a53296b9aded51c
snowb <ammoset@gmail.com>
2019-03-19 13:52:01 +0100

    removed unallowed export

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 process.env.VUE_APP_VERSION=require("child_process").execSync('git rev-list HEAD --count').toString();
 
 module.exports = {
-    publicPath:"./",
-    build_time:new Date().toISOString()
+    publicPath:"./"
 }
