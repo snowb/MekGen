998d94da14baf5388c1961408b4a085c7e0e9f12
snowb <ammoset@gmail.com>
2019-03-20 12:26:15 +0100

    added clean_feat and default_data

--- src/components/data_table_modules/mek_armor/mek_armor-type-data-module.js
+++ src/components/data_table_modules/mek_armor/mek_armor-type-data-module.js
@@ -9,7 +9,7 @@ let armor_type_data_table=
     {type:"Beta",damage_coefficient:4,cost:1.5},
     {type:"Gamma",damage_coefficient:8,cost:2},
 ];
-
+let default_data={type:"Standard",damage_coefficient:1,cost:1};
 //data validator for armor_type_data_table
 let armor_type_validate=(_data)=>
 {
@@ -51,4 +51,37 @@ let get_feature=(_key, _val)=>
     }
 }
 
-export {armor_type_data_table, armor_type_validate, has_feature, get_feature};
+let cleaned_feature=function(_pkey, _feature, _filtered_table)
+{//input: primary key, selected feature, filtered data table
+    let data=undefined;
+    let key_list=[];
+    let update=false;
+    let alerts=[];
+    let json_data=JSON.stringify(_feature);
+
+    if(_feature===undefined || !has_feature(_pkey,_feature[_pkey]))
+    {
+        data=default_data;
+        key_list=[data[_pkey]];
+        update=true;
+        alerts.push("Mek_Armor-Type: "+json_data);
+        alerts.push("**** Invalid data. Reseting to default. ****");
+    }
+    else if(!armor_validate(_feature))
+    {
+        data=JSON.parse(JSON.stringify(get_feature(_pkey,_feature[_pkey])));
+        key_list=[data[_pkey]];
+        update=true;
+        alerts.push("Mek_Armor: "+json_data);
+        alerts.push("**** Invalid data. Reseting. ****");
+    }
+    else
+    {
+        data=JSON.parse(JSON.stringify(_feature));
+        key_list=[data[_pkey]];
+        update=false;
+    }
+    return {data:data, key_list:key_list, update:update, alerts:alerts};
+}
+
+export {armor_type_data_table, armor_type_validate, has_feature, get_feature, cleaned_feature};
