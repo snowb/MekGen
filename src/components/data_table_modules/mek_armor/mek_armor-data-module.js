b77d399a1fb672a2ab8336872d16811de6677466
snowb <ammoset@gmail.com>
2019-03-20 12:13:18 +0100

    add standalone data cleaner

--- src/components/data_table_modules/mek_armor/mek_armor-data-module.js
+++ src/components/data_table_modules/mek_armor/mek_armor-data-module.js
@@ -3,7 +3,8 @@
 import servo_classes_data from "../universal/servo_classes_data.js";
 
 //create new armor_table based on servo_classes_data
-let armor_table=[{name:"None",cost:0,stopping_power:0,code:0}];
+let default_data={name:"None",cost:0,stopping_power:0,code:0};
+let armor_table=[default_data];
 
 let new_armor_table=servo_classes_data.map((_val)=>
 {
@@ -53,4 +54,42 @@ let get_feature=(_key, _val)=>
     }
 }
 
-export {armor_data_table, armor_validate, has_feature, get_feature};
+let cleaned_feature=function(_pkey, _feature, _filtered_table)
+{//input: primary key, selected feature, filtered data table
+    let data=undefined;
+    let key_list=[];
+    let update=false;
+    let alerts=[];
+    let json_data=JSON.stringify(_feature);
+    let filtered_table=_filtered_table===undefined?armor_data_table:_filtered_table;
+    let has_filtered_feature=filtered_table.some((_val)=>
+    {
+        return _val[_pkey]==_feature[_pkey];
+    });
+
+    if(_feature===undefined || !has_feature(_pkey,_feature[_pkey]) || !has_filtered_feature)
+    {
+        data=default_data;
+        key_list=[data[_pkey]];
+        update=true;
+        alerts.push("Mek_Armor: "+json_data);
+        alerts.push("**** Invalid data. Reseting to default. ****");
+    }
+    else if(!armor_validate(_feature))
+    {
+        data=JSON.parse(JSON.stringify(get_feature(_pkey,_feature[_pkey])));
+        key_list=[data[_pkey]];
+        update=true;
+        alerts.push("Mek_Armor: "+json_data);
+        alerts.push("**** Invalid data. Reseting. ****");
+    }
+    else
+    {
+        data=JSON.parse(JSON.stringify(_feature));
+        key_list=[data[_pkey]];
+        update=false;
+    }
+    return {data:data, key_list:key_list, update:update, alerts:alerts};
+}
+
+export {armor_data_table, armor_validate, has_feature, get_feature, cleaned_feature};
