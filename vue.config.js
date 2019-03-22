e40e3b9b4de674101651f9fdc2eaf294cb57e4cf
snowb <ammoset@gmail.com>
2019-03-22 14:37:35 +0100

    change VUE_APP data, commit count and version

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-process.env.VUE_APP_VERSION=require("child_process").execSync('git rev-list HEAD --count').toString();
+process.env.VUE_APP_COMMIT_COUNT=require("child_process").execSync('git rev-list HEAD --count').toString();
+process.env.VUE_APP_VERSION=require("./package.json").version;
 process.env.VUE_APP_BUILD_DATE=require("./timestamp")();
 
 module.exports = {
