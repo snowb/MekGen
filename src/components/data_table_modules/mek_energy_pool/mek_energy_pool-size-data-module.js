eafb23702b8482e6ef6af9393f8e249e392da568
snowb <ammoset@gmail.com>
2019-03-24 14:55:11 +0100

    added default_data and cleaned_feat func

--- src/components/data_table_modules/mek_energy_pool/mek_energy_pool-size-data-module.js
+++ src/components/data_table_modules/mek_energy_pool/mek_energy_pool-size-data-module.js
@@ -13,6 +13,8 @@ let energy_pool_size_data_table=
     {size:Infinity,cost:2},
 ];
 
+let default_data={size:3,cost:1};
+
 //data validator for energy_pool_size_data_table
 let energy_pool_size_validate=(_data)=>
 {
@@ -28,7 +30,7 @@ let energy_pool_size_validate=(_data)=>
                 && _val.damage_capacity==_data.damage_capacity;
     });
     return valid;
-}
+};
 
 let has_feature=(_key, _val)=>
 {
@@ -53,6 +55,39 @@ let get_feature=(_key, _val)=>
         },this);
         return found_feature;
     }
-}
+};
 
-export {energy_pool_size_data_table, energy_pool_size_validate, has_feature, get_feature};
+let cleaned_feature=function(_pkey, _feature)
+{//input: primary key, selected feature, filtered data table
+    let data=undefined;
+    let key_list=[];
+    let update=false;
+    let alerts=[];
+    let json_data=JSON.stringify(_feature);
+
+    if(_feature===undefined || !has_feature(_pkey,_feature[_pkey]))
+    {
+        data=default_data;
+        key_list=[data[_pkey]];
+        update=true;
+        alerts.push("Mek_Energy_Pool-Size: "+json_data);
+        alerts.push("**** Invalid data. Reseting to default. ****");
+    }
+    else if(!energy_pool_size_validate(_feature))
+    {
+        data=JSON.parse(JSON.stringify(get_feature(_pkey,_feature[_pkey])));
+        key_list=[data[_pkey]];
+        update=true;
+        alerts.push("Mek_Energy_Pool-Size: "+json_data);
+        alerts.push("**** Invalid data. Reseting. ****");
+    }
+    else
+    {
+        data=JSON.parse(JSON.stringify(_feature));
+        key_list=[data[_pkey]];
+        update=false;
+    }
+    return {data:data, key_list:key_list, update:update, alerts:alerts};
+};
+
+export {energy_pool_size_data_table, energy_pool_size_validate, has_feature, get_feature, cleaned_feature};
