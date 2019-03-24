a00244a1dd714bdde587993933a1c1033bb474ea
snowb <ammoset@gmail.com>
2019-03-25 00:49:06 +0100

    switch to pkey selector and data module funcs

--- src/components/mek_emw/subcomponents/mek_emw-feature.vue
+++ src/components/mek_emw/subcomponents/mek_emw-feature.vue
@@ -1,22 +1,24 @@
 <template>
     <mek-sub-component-table
-        :items="filteredFeatureTable"
+        :items="feature_table" :pkey="pkey" :selected-keys="selected_keys"
         :headers="{feature:'Feature',cost:'Cost'}"
-        name="Features" flow="col" :show-headers="true"
+        name="Features" flow="pkey-col" :show-headers="true"
         :format="{cost:'multiplier'}"
-        :selected-indices="selected_feature_index_array"
-        @update-selected-indices="select_feature"
+        @update-selected-data="select_feature"
     ></mek-sub-component-table>
 </template>
 <script>
 import selected_item_mixin from "../../../mixins/selected_item_mixin";
 import utility_mixin from "../../../mixins/utility_mixin";
+import alerts_mixin from "../../../mixins/alerts_mixin";
 
+import {feature_data_table, cleaned_feature}
+    from "../../data_table_modules/mek_emw/mek_emw-feature-data-module";
 export default 
 {
     name:"mek_melee_feature",
     props:["featureArray","turnsInUse"],
-    mixins:[selected_item_mixin, utility_mixin],
+    mixins:[selected_item_mixin, utility_mixin,alerts_mixin],
     components:
     {
         "mek-sub-component-table":()=>import("../../universal/mek_sub-component-table.vue")
@@ -24,19 +26,9 @@ export default
     data:function()
     {
         let obj={};
-        obj.feature_table=
-            [
-                {feature:"Rechargeable",cost:1.1},
-                {feature:"Thrown",cost:1.2},
-                {feature:"Quick",cost:2},
-                {feature:"Hyper",cost:7.5},
-                {feature:"Beam Shield",cost:1.5,exclusive_beam_shield:true},
-                {feature:"Variable Beam Shield",cost:2,exclusive_beam_shield:true}
-
-            ];
-
-        obj.exclusive_beam=obj.feature_table.filter((_el)=>{return typeof _el.exclusive_beam_shield!=="undefined";});
-
+        obj.alerts=[];
+        obj.pkey="feature";
+        obj.suppressAlerts=false;
         obj.selected_feature_array=[];
         return obj;
     },
@@ -44,149 +36,76 @@ export default
     {
         select_feature:function(_selected_feature)
         {
-            let selected_feature_name=this.filteredFeatureTable[_selected_feature].feature;
-            let isExclusiveBeam=this.is_exclusive_feature("exclusive_beam",selected_feature_name);
-            let featureClone=Object.assign({},this.filteredFeatureTable[_selected_feature]);
-
-            let temp_selected_feature_array=this.selected_feature_array.filter((_val)=>
-            {//filter out matching feature (toggle)
-                return _val.feature.toLowerCase()!=selected_feature_name.toLowerCase();
-            });
-
-            let togglefeature=this.selected_feature_array.some((_elem)=>
+            this.suppressAlerts=true;//suppress alerts, for exclusive switching
+            let new_selected_feature_array=this.toggleFeature(this.selected_feature_array,_selected_feature);
+            let cleaned_data=cleaned_feature(new_selected_feature_array, this.pkey);
+            new_selected_feature_array=cleaned_data.cleaned_array;
+            if(cleaned_data.alerts.length>0 && !this.suppressAlerts)
             {
-                return _elem.feature.toLowerCase()==selected_feature_name.toLowerCase();
+                cleaned_data.alerts.forEach((_alert)=>
+                {
+                    this.addAlert(_alert);
+                });
+                this.publishAlerts();
+            }
+            this.publishAlerts();
+            this.$set(this,"selected_feature_array",new_selected_feature_array);
+            this.$emit("update-feature",new_selected_feature_array);
+            this.suppressAlerts=false;
+        },
+        toggleFeature(_feature_array,_feature)
+        {//consider moving to universal data module functions thing
+            let feature_array=JSON.parse(JSON.stringify(_feature_array));
+            
+            let remove_feature=feature_array.some((_val)=>
+            {//if _feature matches already existing feature, flag for deletion
+                return _val[this.pkey]==_feature[this.pkey];
             },this);
 
-            if(isExclusiveBeam)
-            {
-                temp_selected_feature_array=temp_selected_feature_array.filter((_val)=>
+            if(remove_feature)
+            {//if flagged for removal, filter out
+                let return_array=feature_array.filter((_val)=>
                 {
-                    return !_val.exclusive_beam_shield
-                })
+                    return _val[this.pkey]!=_feature[this.pkey];
+                },this);
+                return return_array;
             }
-
-            this.$set(this,"selected_feature_array",temp_selected_feature_array);
-
-            if(!togglefeature)
-            {
-                this.selected_feature_array.push(featureClone);
-            }
-            if(temp_selected_feature_array.length==0)
-            {
-                this.$set(this,"selected_feature_array",[]);
-            }
-            this.$emit("update-feature",this.selected_feature_array);
-        },
-        find_feature_index:function(_feature)
-        {
-            let found_index;
-            this.filteredFeatureTable.some(function(_val, _index)
-            {
-                if(_val.feature.toLowerCase() == _feature.toLowerCase())
-                {
-                    found_index=_index;
-                    return true;
-                }
-                return false;
-            });
-
-            return found_index;
-        },
-        is_exclusive_feature:function(_target_array,_feature)
-        {
-            return this[_target_array].some(function(_val)
-            {
-                if(_val.feature.toLowerCase() == _feature.toLowerCase())
-                {
-                    return true;
-                }
-                return false;
-            });
+            feature_array.push(_feature);
+            //otherwise add feature and return
+            return feature_array;
         }
     },
     computed:
     {
-        selected_feature_index_array:function()
+        feature_table()
         {
-            let indices=[];
-            if(this.featureArray.length==0)
+          if(!this.turnsInUse)
             {
-                this.selected_feature_array=[];
-            }
-            if(this.featureArray.length==1)
-            {
-                this.selected_feature_array=[this.filteredFeatureTable[this.find_feature_index(this.featureArray[0].feature)]];
-                return [this.find_feature_index(this.featureArray[0].feature)];
-            }
-            
-            let hasExclusiveBeam=false;
-            let self=this;
-            let feature_list=[];
-            this.selected_feature_array=this.featureArray.reduceRight(function(_prev, _val)
-            {
-                if(self.is_exclusive_feature("exclusive_beam",_val.feature))
-                {
-                    if(!hasExclusiveBeam)
-                    {
-                        _prev.push(_val);
-                        hasExclusiveBeam=true;
-                        feature_list.push(_val.feature.toLowerCase());
-                    }
-                }
-                else if(!feature_list.includes(_val.feature.toLowerCase()))
-                {
-                    _prev.push(_val);
-                    feature_list.push(_val.feature.toLowerCase());
-                }
-                return _prev;
-            },[]);
-
-            indices=this.selected_feature_array.reduce(function(_indices,_val)
-            {
-                _indices.push(self.find_feature_index(_val.feature));
-                return _indices;
-            },[]);
-            if(this.featureArray.length!=this.selected_feature_array.length)
-            {
-                this.$emit("update-feature",this.selected_feature_array);
-            }
-
-            return indices;
-        },
-        flat_feature_array:function()
-        {
-            return this.feature_table.reduce(function(_array, _el)
-            {
-                _array.push(_el.feature);
-                return _array;
-            },[]);
-        },
-        filteredFeatureTable()
-        {
-            if(!this.turnsInUse)
-            {
-                return this.feature_table.filter((_val)=>
+                return feature_data_table.filter((_val)=>
                 {
                     return _val.feature!="Rechargeable";
                 });
             }
-            return this.feature_table;
-        }
-    },
-    watch:
-    {
-        "turnsInUse":function(_newval,_oldval)
+            return feature_data_table;
+        },
+        selected_keys()
         {
-            if(_newval && !_oldval)
+            let cleaned_data=cleaned_feature(this.featureArray,this.pkey);
+            if(cleaned_data.alerts.length>0 && !this.suppressAlerts)
             {
-                let temp_selected_feature_array=this.selected_feature_array.filter((_val)=>
-                {//filter out phalanx selected features if changing turnsInUse from false to true 
-                    return typeof _val!=="undefined" && _val.feature.toLowerCase()!="rechargeable";
-                })
-                this.$emit("update-feature",temp_selected_feature_array);
+                cleaned_data.alerts.forEach((_alert)=>
+                {
+                    this.addAlert(_alert);
+                });
+                this.publishAlerts();
             }
-        }
+            if(cleaned_data.update)
+            {
+                this.$emit("update-feature",cleaned_data.cleaned_array);
+            }
+            this.$set(this,"selected_feature_array",cleaned_data.cleaned_array);
+            return cleaned_data.key_list;
+        },
     }
 }
 </script>
