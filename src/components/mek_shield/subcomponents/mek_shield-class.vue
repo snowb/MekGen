dc5d346025e6fcec680c49eb15553c4f8793f883
snowb <ammoset@gmail.com>
2019-03-25 11:42:47 +0100

    switch to pkey selector, module funcs, code removal

--- src/components/mek_shield/subcomponents/mek_shield-class.vue
+++ src/components/mek_shield/subcomponents/mek_shield-class.vue
@@ -1,22 +1,23 @@
 <template>
     <mek-sub-component-table
-        :items="class_list"
+        :items="class_table" :pkey="pkey" :selected-keys="selected_keys"
         :headers="headers"
-        name="Class" flow="col" :show-headers="true"
-        :selected-indices="shield_class_index"
-        @update-selected-indices="select_class"
+        name="Class" flow="pkey-col" :show-headers="true"
+        @update-selected-data="select_class"
     ></mek-sub-component-table>
 </template>
 <script>
 import servo_classes_mixin from "../../../mixins/servo_classes_mixin.js";
-import selected_item_mixin from "../../../mixins/selected_item_mixin.js";
 import utility_mixin from "../../../mixins/utility_mixin.js";
+import alerts_mixin from "../../../mixins/alerts_mixin.js";
+import { class_data_table, cleaned_feature, create_class_data_table } 
+    from '../../data_table_modules/mek_shield/mek_shield-class-data-module';
 
 export default 
 {
     name:"mek_shield_class",
     props:["type","shield_class","is_ablative"],
-    mixins:[servo_classes_mixin,selected_item_mixin,utility_mixin],
+    mixins:[servo_classes_mixin,utility_mixin,alerts_mixin],
     components:
     {
         "mek-sub-component-table":()=>import("../../universal/mek_sub-component-table.vue")
@@ -24,99 +25,25 @@ export default
     data:function()
     {
         let obj={};
+        obj.alerts=[];
+        obj.pkey="stopping_power";
+        obj.suppressAlerts=false;
         return obj;
     },
     methods:
     {
-        select_class:function(_class_code)
+        select_class:function(_class_data)
         {
-            this.$emit("update-class-code",this.class_list[_class_code]);
-        },
-        find_class_code_index:function(_class_code)
-        {
-            let class_index=null;
-            this.servo_classes.some(function(_val,_index)
-            {
-                if(_val.code==_class_code)
-                {
-                    class_index=_index;
-                    return true;
-                }
-            },this);
-            return class_index;
-        },
-        calculate_cost:function(_class_code)
-        {
-            let type_multiplier=1;//Standard
-            switch(this.type.toLowerCase())
-            {
-                case "active":
-                    type_multiplier=1.5
-                    break;
-                case "reactive":
-                    type_multiplier=3;
-                    break;
-            }
-            return this.calculate_stopping_power(_class_code) * type_multiplier;
-        },
-        calculate_stopping_power:function(_class_code)
-        {
-            return _class_code + 4;
+            let data=JSON.parse(JSON.stringify(_class_data));
+            this.$emit("update-class-code",data);
         }
     },
     computed:
     {
-        shield_class_index:function()
+        class_table()
         {
-            let class_index=0;
-            this.class_list.some((_val,_index)=>
-            {
-                if(this.shield_class.code==_val.code)
-                {
-                    class_index=_index;
-                    return true;
-                }
-            });
-
-            let update=false;
-            switch(true)
-            {
-                case this.shield_class.id!=this.class_list[class_index].id:
-                case this.shield_class.name!=this.class_list[class_index].name:
-                case this.shield_class.stopping_power!=this.class_list[class_index].stopping_power:
-                case this.shield_class.cost!=this.class_list[class_index].cost:
-                    update=true;       
-            }
-            if(update)
-            {   
-                this.select_class(class_index);
-            }
-            return [class_index];
-        },
-        flat_name_array:function()
-        {
-            return this.servo_classes.reduce(function(_array, _el)
-            {
-                _array.push(_el.name);
-                return _array;
-            },[]);
-        },
-        class_list()
-        {
-            this.type;//accessing type to cause recompute when type updates
-            return this.servo_classes.map(function(_val)
-            {
-                let newelement=
-                {
-                    stopping_power:_val.code+4,
-                    code:_val.code,
-                    id:_val.id,
-                    name:_val.name,
-                    kills:(_val.code+4)*5,
-                    cost:this.calculate_cost(_val.code)
-                };
-                return newelement;
-            },this);
+            create_class_data_table(this.type);
+            return class_data_table;
         },
         headers()
         {
@@ -125,6 +52,23 @@ export default
                 return {name:'Name',kills:'Kills',cost:'Cost'};
             }
             return {name:'Name',stopping_power:'SP',cost:'Cost'};
+        },
+        selected_keys()
+        {
+            let cleaned_data=cleaned_feature(this.pkey, this.shield_class);
+            if(cleaned_data.alerts.length>0 && !this.suppressAlerts)
+            {
+                cleaned_data.alerts.forEach((_alert)=>
+                {
+                    this.addAlert(_alert);
+                });
+                this.publishAlerts();
+            }
+            if(cleaned_data.update)
+            {
+                this.select_class(cleaned_data.data);
+            }
+            return cleaned_data.key_list;
         }
     }
 }
