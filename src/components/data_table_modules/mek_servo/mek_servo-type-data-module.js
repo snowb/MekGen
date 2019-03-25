2f32fd6613ed2953e91035f4710f839b534ba585
snowb <ammoset@gmail.com>
2019-03-25 23:40:55 +0100

    switch to partial functions module

--- src/components/data_table_modules/mek_servo/mek_servo-type-data-module.js
+++ src/components/data_table_modules/mek_servo/mek_servo-type-data-module.js
@@ -1,6 +1,7 @@
 //data table module, raw data output for re-use in non-vue-component formats
+import {partial_validate, partial_has_feature, partial_get_feature, partial_cleaned_feature} from "../universal/mek_partial-function-data-module";
 
-//create new ammo_data_table
+//create new servo_data_table
 let servo_data_table=
 [
     {type:"Torso"},
@@ -11,79 +12,20 @@ let servo_data_table=
     {type:"Tail"},
     {type:"Pod"},
 ];
-
+let data_table_keys=["type"];
 let default_data={type:"Torso"};
 
-//data validator for ammo_data_table
-let servo_type_validate=(_data)=>
-{
-    if(typeof _data==="undefined")
-    {
-        return false;
-    }
-    let valid=servo_data_table.some((_val)=>
-    {
-        return _val.type==_data.type;
-    });
-    return valid;
-}
+//data validator for servo_data_table
+//call partial_validate with appropriate data for full validate
+let servo_type_validate=partial_validate(servo_data_table, data_table_keys);
 
-let has_feature=(_key, _val)=>
-{
-    return servo_data_table.some((_data)=>
-    {
-        return _data[_key]!==undefined && _data[_key]==_val;
-    });
-};
+//completed function for checking if data has data
+let has_feature=partial_has_feature(servo_data_table);
 
-let get_feature=(_key, _val)=>
-{
-    if(has_feature(_key,_val))
-    {
-        let found_feature=null;
-        servo_data_table.some((_table_val)=>
-        {
-            if(_table_val[_key]==_val)
-            {
-                found_feature=_table_val;
-                return true;
-            }
-        },this);
-        return found_feature;
-    }
-}
+//completed function for returning matching data
+let get_feature=partial_get_feature(servo_data_table, has_feature);
 
-let cleaned_feature=function(_pkey, _feature)
-{//input: primary key, selected feature, filtered data table
-    let data=undefined;
-    let key_list=[];
-    let update=false;
-    let alerts=[];
-    let json_data=JSON.stringify(_feature);
-
-    if(_feature===undefined || !has_feature(_pkey,_feature[_pkey]))
-    {
-        data=default_data;
-        key_list=[data[_pkey]];
-        update=true;
-        alerts.push("Mek_Servo-Class: "+json_data);
-        alerts.push("**** Invalid data. Reseting to default. ****");
-    }
-    else if(!servo_type_validate(_feature))
-    {
-        data=JSON.parse(JSON.stringify(get_feature(_pkey,_feature[_pkey])));
-        key_list=[data[_pkey]];
-        update=true;
-        alerts.push("Mek_Servo-Class: "+json_data);
-        alerts.push("**** Invalid data. Reseting. ****");
-    }
-    else
-    {
-        data=JSON.parse(JSON.stringify(_feature));
-        key_list=[data[_pkey]];
-        update=false;
-    }
-    return {data:data, key_list:key_list, update:update, alerts:alerts};
-}
+//completed function for returning cleaed data
+let cleaned_feature=partial_cleaned_feature(servo_type_validate, has_feature, get_feature, default_data, "Mek_Servo-Type");
 
 export {servo_data_table, servo_type_validate, has_feature, get_feature, cleaned_feature};
