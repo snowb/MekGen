18884ff70f73b2702d988817f992eda8aff75017
snowb <ammoset@gmail.com>
2019-03-28 23:53:12 +0100

    refac pkey select, module, alerts

--- src/components/mek_projectile/subcomponents/mek_projectile-feature.vue
+++ src/components/mek_projectile/subcomponents/mek_projectile-feature.vue
@@ -1,22 +1,22 @@
 <template>
     <mek-sub-component-table
-        :items="filteredFeatureTable"
+        :items="filteredFeatureTable" :pkey="pkey" :selected-keys="selected_keys"
         :headers="{feature:'Feature',cost:'Cost'}" :show-headers="true" :format="{cost:'multiplier'}"
-        :selected-indices="selected_feature_index_array"
-        @update-selected-indices="select_feature"
-        name="Features"
-        flow="col"
+        flow="pkey-col" name="Features"
+        @update-selected-data="select_feature"
     ></mek-sub-component-table>
 </template>
 <script>
-import selected_item_mixin from "../../../mixins/selected_item_mixin";
-import utility_mixin from "../../../mixins/utility_mixin";
+import alerts_mixin from "../../../mixins/alerts_mixin";
+
+import {feature_data_table, cleaned_feature, filter_data_table} 
+    from "../../data_table_modules/mek_projectile/mek_projectile-feature-data-module.js";
 
 export default 
 {
     name:"mek_projectile_feature",
     props:["featureArray","burstValue"],
-    mixins:[selected_item_mixin, utility_mixin],
+    mixins:[alerts_mixin],
     components:
     {
         "mek-sub-component-table":()=>import("../../universal/mek_sub-component-table.vue")
@@ -24,216 +24,88 @@ export default
     data:function()
     {
         let obj={};
-        obj.feature_table=
-            [
-                {feature:"Long Range",cost:1.33},
-                {feature:"Hypervelocity",cost:1.25},
-                {feature:"Phalanx",cost:0.5,exclusive_phalanx:true},
-                {feature:"Variable Phalanx",cost:1.4,exclusive_phalanx:true},
-                {feature:"Anti-Personnel",cost:1.0,exclusive_personnel:true},
-                {feature:"Variable Anti-Personnel",cost:1.8,exclusive_personnel:true},
-                {feature:"Anti-Personnel Phalanx",cost:1.6,exclusive_personnel:true,exclusive_phalanx:true},
-                {feature:"All Purpose",cost:2.6,exclusive_personnel:true,exclusive_phalanx:true}
-
-            ];
-
-        obj.exclusive_phalanx=obj.feature_table.filter((_el)=>{return typeof _el.exclusive_phalanx!=="undefined";});
-        obj.exclusive_personnel=obj.feature_table.filter((_el)=>{return typeof _el.exclusive_personnel!=="undefined";});
-
+        obj.pkey="feature";
+        obj.alerts=[];
+        obj.suppressAlerts=false;
         obj.selected_feature_array=[];
         return obj;
     },
     methods:
     {
-        select_feature:function(_selected_feature_index)
-        {
-            this.burstValue;
-            let select_feature_name=this.filteredFeatureTable[_selected_feature_index].feature;
-            let isExclusivePhalanx=this.is_exclusive_feature("exclusive_phalanx",select_feature_name);
-            let isExclusivePersonnel=this.is_exclusive_feature("exclusive_personnel",select_feature_name);
-            let featureClone=Object.assign({},this.filteredFeatureTable[_selected_feature_index]);
-
-            let temp_selected_feature_array=this.selected_feature_array.filter((_val)=>
-            {//filter out matching feature (toggle)
-                return _val.feature.toLowerCase()!=select_feature_name.toLowerCase();
-            });
-
-            let togglefeature=this.selected_feature_array.some((_elem)=>
+        select_feature:function(_selected_feature)
+        {//refactor to external feature array cleaner
+            this.suppressAlerts=true;//suppress alerts, for exclusive switching
+            let new_selected_feature_array=this.toggleFeature(this.selected_feature_array,_selected_feature);
+            let cleaned_data=cleaned_feature(new_selected_feature_array, this.pkey);
+            new_selected_feature_array=cleaned_data.cleaned_array;
+            if(cleaned_data.alerts.length>0 && !this.suppressAlerts)
             {
-                return _elem.feature.toLowerCase()==select_feature_name.toLowerCase();
+                cleaned_data.alerts.forEach((_alert)=>
+                {
+                    this.addAlert(_alert);
+                });
+                this.publishAlerts();
+            }
+            this.$set(this,"selected_feature_array",new_selected_feature_array);
+            this.$emit("update-feature",new_selected_feature_array);
+            this.suppressAlerts=false;
+        },
+        toggleFeature(_feature_array,_feature)
+        {
+            let feature_array=JSON.parse(JSON.stringify(_feature_array));
+            
+            let remove_feature=feature_array.some((_val)=>
+            {//if _feature matches already existing feature, flag for deletion
+                return _val[this.pkey]==_feature[this.pkey];
             },this);
 
-            if(isExclusivePhalanx && isExclusivePersonnel)
-            {//filter out exclusive phalanx and personnel
-                temp_selected_feature_array=temp_selected_feature_array.filter((_val)=>
+            if(remove_feature)
+            {//if flagged for removal, filter out
+                return feature_array.filter((_val)=>
                 {
-                    return !_val.exclusive_phalanx && !_val.exclusive_personnel;
-                })
+                    return _val[this.pkey]!=_feature[this.pkey];
+                },this);
             }
-            if(isExclusivePhalanx)
-            {//filter out exclusive phalanx
-                temp_selected_feature_array=temp_selected_feature_array.filter((_val)=>
-                {
-                    return !_val.exclusive_phalanx;
-                })
-            }
-            if(isExclusivePersonnel)
-            {//filter out exclusive personnel
-                temp_selected_feature_array=temp_selected_feature_array.filter((_val)=>
-                {
-                    return !_val.exclusive_personnel;
-                })
-            }
-
-            if(!togglefeature)
-            {
-                temp_selected_feature_array.push(featureClone);
-            }
-            
-            this.$set(this,"selected_feature_array",temp_selected_feature_array);
-            
-            if(temp_selected_feature_array.length==0)
-            {
-                this.$set(this,"selected_feature_array",[]);
-            }
-
-            this.$emit("update-feature",this.selected_feature_array);
-        },
-        find_feature_index:function(_feature)
-        {
-            let found_index;
-            this.filteredFeatureTable.some(function(_val, _index)
-            {
-                if(_val.feature.toLowerCase() == _feature.toLowerCase())
-                {
-                    found_index=_index;
-                    return true;
-                }
-                return false;
-            });
-
-            return found_index;
-        },
-        is_exclusive_feature:function(_target_array,_feature)
-        {
-            return this[_target_array].some(function(_val)
-            {
-                if(_val.feature.toLowerCase() == _feature.toLowerCase())
-                {
-                    return true;
-                }
-                return false;
-            });
-        },
-        exclusive_indices:function()
-        {
-            let foundIndices=this.selected_feature_array.reduce(function(_indices,_val,_index)
-            {
-                if(_val.exclusive)
-                {
-                    _indices.push(_index);
-                }
-                return _indices;
-            },[]);
-            return foundIndices;
+            feature_array.push(_feature);
+            //otherwise add feature and return
+            return feature_array;
         }
     },
     computed:
     {
-        selected_feature_index_array:function()
-        {
-            let indices=[];
-
-            if(this.featureArray.length==0)
-            {
-                this.selected_feature_array=[];
-            }
-            if(this.featureArray.length==1)
-            {
-                this.selected_feature_array=[this.filteredFeatureTable[this.find_feature_index(this.featureArray[0].feature)]];
-                return [this.find_feature_index(this.featureArray[0].feature)];
-            }
-
-            let hasExclusivePhalanx=false;
-            let hasExclusivePersonnel=false;
-            let self=this;
-            let feature_list=[];
-
-            this.selected_feature_array=this.featureArray.reduceRight(function(_prev, _val)
-            {
-                let isPhalanx=self.is_exclusive_feature("exclusive_phalanx",_val.feature);
-                let isPersonnel=self.is_exclusive_feature("exclusive_personnel",_val.feature);
-                if( isPhalanx && !hasExclusivePhalanx && isPersonnel && !hasExclusivePersonnel)
-                {
-                    _prev.push(_val);
-                    hasExclusivePhalanx=true;
-                    hasExclusivePersonnel=true;
-                    feature_list.push(_val.feature.toLowerCase());
-                }
-                if(isPhalanx && !hasExclusivePhalanx)
-                {
-                    _prev.push(_val);
-                    hasExclusivePhalanx=true;
-                    feature_list.push(_val.feature.toLowerCase());
-                }
-                if(isPersonnel && !hasExclusivePersonnel)
-                {
-                    _prev.push(_val);
-                    hasExclusivePersonnel=true;
-                    feature_list.push(_val.feature.toLowerCase());
-                }
-                if(!feature_list.includes(_val.feature.toLowerCase()))
-                {
-                    _prev.push(_val);
-                    feature_list.push(_val.feature.toLowerCase());
-                }
-                return _prev;
-            },[]);
-
-            indices=this.selected_feature_array.reduce(function(_indices,_val)
-            {
-                _indices.push(self.find_feature_index(_val.feature));
-                return _indices;
-            },[]);
-            if(this.featureArray.length!=this.selected_feature_array.length)
-            {
-                this.$emit("update-feature",this.selected_feature_array);
-            }
-            this.burstValue;
-
-            return indices;
-        },
-        flat_feature_array:function()
-        {
-            return this.feature_table.reduce(function(_array, _el)
-            {
-                _array.push(_el.feature);
-                return _array;
-            },[]);
-        },
         filteredFeatureTable()
         {
-            if(this.burstValue<=1)
+            filter_data_table(this.burstValue);
+            return feature_data_table;
+        },
+        selected_keys()
+        {
+            this.burstValue;
+            let cleaned_data=cleaned_feature(this.featureArray,this.pkey);
+            if(cleaned_data.alerts.length>0 && !this.suppressAlerts)
             {
-                return this.feature_table.filter((_val)=>
+                cleaned_data.alerts.forEach((_alert)=>
                 {
-                    return !this.is_exclusive_feature("exclusive_phalanx",_val.feature);// || this.is_exclusive_feature("exclusive_personnel",_val.feature) ;
-                },this);
+                    this.addAlert(_alert);
+                });
+                this.publishAlerts();
             }
-            return this.feature_table;
+            if(cleaned_data.update)
+            {
+                this.$emit("update-feature",cleaned_data.cleaned_array);
+            }
+            this.$set(this,"selected_feature_array",cleaned_data.cleaned_array);
+            this.suppressAlerts=false;
+            return cleaned_data.key_list;
         }
     },
     watch:
     {
         "burstValue":function(_newval,_oldval)
-        {
-            if(_newval<=1 && _oldval>1)
+        {//must track changes in burstValue to disable alerts for removed features on cleaned_feat
+            if(_newval!=_oldval)
             {
-                let temp_selected_feature_array=this.selected_feature_array.filter((_val)=>
-                {//filter out phalanx selected features if changing from BV>1 to BV=1
-                    return !_val.exclusive_phalanx;
-                })
-                this.$emit("update-feature",temp_selected_feature_array);
+                this.suppressAlerts=true;
             }
         }
     }
