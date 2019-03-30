15c45bbc36685a4dfc91a3d5050819cb38ed2fa2
snowb <ammoset@gmail.com>
2019-03-30 23:40:29 +0100

    re-order of functions

--- src/components/data_table_modules/mek_beam/mek_beam-shots-data-module.js
+++ src/components/data_table_modules/mek_beam/mek_beam-shots-data-module.js
@@ -18,33 +18,6 @@ let data_cached=false;
 let data_table_keys=["shots","cost"];
 let default_data={shots:"__INFINITY__",cost:1};
 
-let filter_shots_data_table=(_mag_fed)=>
-{
-    if(!_mag_fed)
-    {
-        shots_data_table=raw_shots_data_table.filter((_val)=>
-        {
-            return _val.shots!=15;
-        });
-        default_data={shots:"__INFINITY__",cost:1};
-    }
-    else
-    {
-        shots_data_table=raw_shots_data_table.filter((_val)=>
-        {
-            return _val.shots!=0 && _val.shots!="__INFINITY__";
-        });
-        default_data={shots:15,cost:1};
-    }
-    //reset all functions for new table
-    has_feature=partial_has_feature(shots_data_table);
-    get_feature=partial_get_feature(shots_data_table, has_feature);
-    cleaned_feature=
-        partial_cleaned_feature(shots_validate, has_feature, get_feature, default_data, "Mek_Beam-Shots");
-    data_cached=false;
-};
-filter_shots_data_table(false);
-
 let cached_validate=partial_validate(shots_data_table, data_table_keys);
 //data validator for shots_data_table
 //call partial_validate with appropriate data for full validate
@@ -68,5 +41,32 @@ let get_feature=partial_get_feature(shots_data_table, has_feature);
 //completed function for returning cleaed data
 let cleaned_feature=
     partial_cleaned_feature(shots_validate, has_feature, get_feature, default_data, "Mek_Beam-Shots");
+
+    let filter_shots_data_table=(_mag_fed)=>
+    {
+        if(!_mag_fed)
+        {
+            shots_data_table=raw_shots_data_table.filter((_val)=>
+            {
+                return _val.shots!=15;
+            });
+            default_data={shots:"__INFINITY__",cost:1};
+        }
+        else
+        {
+            shots_data_table=raw_shots_data_table.filter((_val)=>
+            {
+                return _val.shots!=0 && _val.shots!="__INFINITY__";
+            });
+            default_data={shots:15,cost:1};
+        }
+        //reset all functions for new table
+        has_feature=partial_has_feature(shots_data_table);
+        get_feature=partial_get_feature(shots_data_table, has_feature);
+        cleaned_feature=
+            partial_cleaned_feature(shots_validate, has_feature, get_feature, default_data, "Mek_Beam-Shots");
+        data_cached=false;
+    };
+    filter_shots_data_table(false);
 
 export {shots_data_table, shots_validate, has_feature, get_feature, cleaned_feature, filter_shots_data_table};
