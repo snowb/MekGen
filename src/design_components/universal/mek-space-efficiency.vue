0e9f7f4fb5e5940cef206fc4433332053f91b8fd
snowb <ammoset@gmail.com>
2019-03-31 12:52:08 +0200

    added styles in .Vue

--- src/design_components/universal/mek-space-efficiency.vue
+++ src/design_components/universal/mek-space-efficiency.vue
@@ -135,3 +135,46 @@ export default
  * 
 */
 </script>
+<style scoped>
+.metallic_background_small
+{
+    background-image: linear-gradient(to left top, rgba(247,0,0,1) 0%, 
+        rgba(255, 129, 110,1) 49%, 
+        rgba(255, 129, 110,1) 51%, 
+        rgba(185,0,0,1) 100%);
+    height: 100%;
+    width: 100%;
+    padding:5px;
+    border-radius: 7px;
+    box-shadow: #222 0px 0px 0px 2px, #fff 0px 0px 5px 2px;
+    padding-top:34px;
+}
+.subsection_container
+{
+    position: relative;
+    border-radius: 7px;
+    padding: 10px;
+    background-color: rgb(170, 170, 170);
+    height: 100%;
+    width: 100%;
+    box-shadow: rgb(34, 34, 34) 0px 0px 0px 2px inset, rgb(255, 255, 255) 0px 0px 5px 2px inset;
+}
+.subsection_header_small
+{
+    font-weight: bold;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%,0%);
+    top: -32px;
+    font-family: Arial Black, sans-serif;
+    font-size: 110%;
+    border: 1px solid black;
+    padding: 2px 10px;
+    border-radius: 7px;
+    box-shadow: rgba(255, 255, 255, 0.5) -1px -1px 1px 1px inset, rgba(0,0,0,0.5) 1px 1px 1px 3px inset;
+    color: #ddd;
+    text-shadow: 1px 1px 1px #000, 1px -1px 1px #000, -1px -1px 1px #000, -1px 1px 1px #000;
+    background-color: rgba(0,0,0,0.2);
+    white-space: nowrap;
+}
+</style>
