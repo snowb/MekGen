714261fd8912a12aff70372c7d514125ba629422
snowb <ammoset@gmail.com>
2019-04-09 19:12:06 +0200

    create export-type-menu

---
+++ src/export_components/sub_components/mek_export-type-menu.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="app" style="display:inline-flex-col;">
+        <mek-top-menu @focus-section="focusSection" :section="targetTypeTab" :section-list="sectionList"
+            size="sm" color="d" color-offset="rgb"
+        ></mek-top-menu>
+        <mek-component-menu v-if="targetTypeTab!=''"></mek-component-menu>
+    </div>
+</template>
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+  name: 'export-type-menu',
+  components:
+  {
+    "mek-top-menu":()=>import(/* webpackChunkName: "mek_top-menu" */"../../app_components/mek_top_menu/mek_top-menu.vue"),
+    "mek-component-menu":()=>import(/* webpackChunkName: "mek_component-menu" */"./mek_export-component-menu.vue"),
+  },
+  data:function()
+  {
+    let obj={};
+    return obj;
+  },
+  methods:
+  {
+    focusSection:function(_section)
+    {
+        this.$store.commit("showTab",{prop:"currentTypeTab",tab:_section});
+    }
+  },
+  computed:
+  {
+    sectionList()
+    {
+      return this.typeList(this.targetExportTab).map((_val)=>
+      {
+        let name=_val.charAt(0).toUpperCase()+_val.slice(1);
+        return {id:_val,name:name};
+      });
+    },
+    ...mapGetters(
+        {
+            targetTypeTab:'targetTypeTab',
+            targetExportTab:'targetExportTab',
+            typeList:'typeList',
+            getComponentByType:'getComponentByType'
+        })
+  }
+}
+</script>
+
+<style>
+</style>
