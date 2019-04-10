914649d7da581e914c8e385f4c5bb733761a314c
snowb <ammoset@gmail.com>
2019-04-10 20:01:31 +0200

    show perm icons, switch save to click only

--- src/design_components/universal/mek-component-name.vue
+++ src/design_components/universal/mek-component-name.vue
@@ -1,20 +1,25 @@
 <template>
-    <div style="margin:10px 0px;align-self:flex-start;" @mouseover="showEdit=true" @mouseout="showEdit=false">
+    <div class="container">
         <span class="section_header">
-            <span style="font-size:50%;vertical-align:super;">{{newComponent?"(New)":""}} {{componentChanged?"(Unsaved)":""}} </span>
+            <span class="info_tag">{{newComponent?"(New)":""}} {{componentChanged?"(Unsaved)":""}} </span>
             <span class="edit_name" :contenteditable="editMode" @keydown="catchEnter($event)">{{componentName}}</span>
         </span>
-        <span v-if="showEdit && !editMode" class="icon_container edit_icon" title="Edit Name" @click="editName">
-            <v-icon name="edit" scale="2"></v-icon>
+        <span v-if="showEdit && !editMode" class="icon_container edit_icon" title="Edit Name" 
+            @click="editName">
+            <v-icon name="edit" scale="1"></v-icon>
         </span>
         <span v-if="editMode" class="icon_container save_icon" title="Save Name" @click="saveName">
-            <v-icon name="save" scale="2"></v-icon>
+            <v-icon name="save" scale="0.75"></v-icon>
+        </span>
+        <span v-if="editMode" class="icon_container cancel_icon" title="Cancel" @click="cancelEdit">
+            <v-icon name="window-close" scale="0.75"></v-icon>
         </span>
     </div>
 </template>
 <script>
 import 'vue-awesome/icons/edit';
 import 'vue-awesome/icons/save';
+import 'vue-awesome/icons/window-close';
 import Icon from 'vue-awesome/components/Icon';
 
 export default
@@ -28,8 +33,9 @@ export default
     data:function()
     {
         let obj={};
-        obj.showEdit=false;
+        obj.showEdit=true;
         obj.editMode=false;
+        obj.newName="";
         return obj;
     },
     methods:
@@ -56,6 +62,13 @@ export default
             let editTargetContent=document.querySelector(".edit_name").textContent;
             this.$emit("update-component-name",editTargetContent)
         },
+        cancelEdit()
+        {
+            this.editMode=false;
+            document.querySelector(".edit_name").innerText=this.componentName;
+            document.querySelector(".edit_name").innerHTML=this.componentName;
+            document.querySelector(".edit_name").textContent=this.componentName;
+        },
         catchEnter(_event)
         {
             switch(true)
@@ -66,7 +79,7 @@ export default
                 case /^Enter$/i.test(_event.key):
                     _event.preventDefault();
                     _event.stopPropagation();
-                    this.saveName();
+                    //this.saveName();
                     break;
                 default:
                     _event.preventDefault();
@@ -77,6 +90,12 @@ export default
 }
 </script>
 <style scoped>
+.container
+{
+    margin:10px 0px;
+    align-self:flex-start;
+    position: relative;
+}
 .icon_container
 {
     /*background-image: linear-gradient(to left top, rgba(247,0,0,1) 0%, 
@@ -91,13 +110,25 @@ export default
     position: relative;
     cursor: pointer;
 }
+.info_tag
+{
+    font-size:50%;
+    vertical-align:super;
+}
 .edit_icon
 {
-    top:3px;
+    top: 5px;
+    position: absolute;
 }
 .save_icon
 {
-    top:5px;
+    top: 5px;
+    position: absolute;
+}
+.cancel_icon
+{
+    bottom: 5px;
+    position: absolute;
 }
 .section_header
 {
