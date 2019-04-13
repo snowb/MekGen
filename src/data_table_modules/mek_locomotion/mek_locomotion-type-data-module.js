129c32547b7a7c4f4fbda54a74615d4b40fd5365
snowb <ammoset@gmail.com>
2019-04-13 15:22:49 +0200

    created loco type module and funcs

---
+++ src/data_table_modules/mek_locomotion/mek_locomotion-type-data-module.js
@@ -0,0 +1,28 @@
+//data table module, raw data output for re-use in non-vue-component formats
+import {partial_validate, partial_has_feature, partial_get_feature, partial_cleaned_feature} 
+    from "@/data_table_modules/universal/mek_partial-function-data-module";
+
+//create new locomotion_data_table
+let locomotion_data_table=
+[
+    {type:"Wheels"},
+    {type:"Treads"},
+];
+let data_table_keys=["type"];
+let default_data={type:"Wheel"};
+
+//data validator for locomotion_data_table
+//call partial_validate with appropriate data for full validate
+let locomotion_type_validate=partial_validate(locomotion_data_table, data_table_keys);
+
+//completed function for checking if data has data
+let has_feature=partial_has_feature(locomotion_data_table);
+
+//completed function for returning matching data
+let get_feature=partial_get_feature(locomotion_data_table, has_feature);
+
+//completed function for returning cleaed data
+let cleaned_feature=
+    partial_cleaned_feature(locomotion_type_validate, has_feature, get_feature, default_data, "Mek_Locomotion-Type");
+
+export {locomotion_data_table, locomotion_type_validate, has_feature, get_feature, cleaned_feature};
