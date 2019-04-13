8ab7fc8ab133fbf38f8098a30a954330ef979688
snowb <ammoset@gmail.com>
2019-04-13 08:55:26 +0200

    creation of import component

--- src/import_components/mek_import-component.vue
+++ src/import_components/mek_import-component.vue
@@ -1,63 +1,128 @@
 <template>
     <div id="app" style="display:inline-flex-col;">
-        <span>
-            <!--mek-top-menu @focus-section="focusSection" :section="targetBuildTab" :section-list="sectionList"></mek-top-menu>
-            <span id="design-main">
-                <component :is="targetDesignTab"></component>
-            </span-->
-            Ingest Component
+      <div class="json_container" contenteditable="true" @input="updateJSON($event.target.textContent)">
+      </div>
+      <div class="mek-button-container">
+        <span :class="{'mek-button':!importClicked,'mek-button-clicked':importClicked}" 
+            @click="importJSON" @animationend="importClicked=false"
+            title="Save Equipment"
+        >
+            Import
         </span>
-        <!--mek-alert></mek-alert-->
+      </div>
     </div>
 </template>
 <script>
-
-import {mapGetters} from 'vuex';
 
 export default {
   name: 'import-component',
   components:
   {
-    //"mek-top-menu":()=>import(/* webpackChunkName: "mek_top-menu" */"../app_components/mek_top_menu/mek_top-menu.vue"),
-    //"mek-side-menu":()=>import(/* webpackChunkName: "side-menu" */"./mek_side_menu/mek_side-menu.vue"),
-    //"mek-alert":()=>import(/* webpackChunkName: "mek_alert" */"../app_components/universal/mek_alert.vue"),
-
-    //"mek-servo":()=>import(/* webpackChunkName: "mek_servo-main" */"./mek_servo/mek_servo-main.vue"),
-    //"mek-melee":()=>import(/* webpackChunkName: "mek_melee-main" */"./mek_melee/mek_melee-main.vue"),
-    //"mek-projectile":()=>import(/* webpackChunkName: "mek_projectile-main" */"./mek_projectile/mek_projectile-main.vue"),
-    //"mek-magazine":()=>import(/* webpackChunkName: "mek_magazine-main" */"./mek_magazine/mek_magazine-main.vue"),
-    //"mek-missile":()=>import(/* webpackChunkName: "mek_missile-main" */"./mek_missile/mek_missile-main.vue"),
-    //"mek-emw":()=>import(/* webpackChunkName: "mek_emw-main" */"./mek_emw/mek_emw-main.vue"),
-    //"mek-beam":()=>import(/* webpackChunkName: "mek_beam-main" */"./mek_beam/mek_beam-main.vue"),
-    //"mek-energy-pool":()=>import(/* webpackChunkName: "mek_energy_pool-main" */"./mek_energy_pool/mek_energy_pool-main.vue"),
-    //"mek-shield":()=>import(/* webpackChunkName: "mek_shield-main" */"./mek_shield/mek_shield-main.vue"),
-    //"mek-reflector":()=>import(/* webpackChunkName: "mek_reflector-main" */"./mek_reflector/mek_reflector-main.vue")
   },
   data:function()
   {
     let obj={};
-    obj.sectionList=
-    [
-        {id:"mek-nothing",name:"Nothing"},
-    ];
+    obj.jsonData="";
+    obj.importClicked=false;
     return obj;
   },
   methods:
   {
-    focusSection:function(_section)
+    updateJSON(_data)
     {
-        this.$store.commit("showTab",{prop:"currentImportTab",tab:_section});
+      this.jsonData=_data;
+    },
+    importJSON()
+    {
+      this.importClicked=true;
+      this.$store.commit("saveComponent",JSON.parse(this.jsonData.replace(/\s/g,"")));
+      this.jsonData="";
     }
-  },
-  computed:
-  {
-    ...mapGetters(
-        {
-            targetDesignTab:'targetIngestTab'
-        })
   }
 }
 </script>
 
-<style>
+<style scoped>
+.json_container
+{
+    align-self: baseline;
+    display:inline-flex;
+    flex-direction:column;
+    border-radius: 7px;
+    padding: 10px;
+    background-color: #eee; /* rgb(170, 170, 170); */
+    height: 100%;
+    -webkit-box-shadow: rgb(34, 34, 34) 0px 0px 0px 2px inset, rgb(0, 0, 0) 0px 0px 5px 2px inset;
+    box-shadow: rgb(34, 34, 34) 0px 0px 0px 2px inset, rgb(0, 0, 0) 0px 0px 5px 2px inset;
+    margin-right: 10px;
+    max-width: 75vw;
+    overflow-wrap:break-word;
+    word-break: break-all;
+    text-align:left;
+    min-width: 10vw;
+}
+@media all and (max-width:500px)
+{
+    .mek-button-container
+    {
+        display:contents;
+    }
+}
+.mek-button-container
+{
+    align-self:center;
+    font-size: 150%;
+    font-weight: bold;
+    margin:5px;
+    margin-top: 10px;
+}
+.mek-button
+{
+    padding: 5px 15px 5px 15px;
+    margin: 5px;
+    border: 1px solid #222;
+
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.7);
+    color: #e2e2e2;
+    border-radius: 30px;
+    box-shadow: inset -1px -1px 1px 1px white;
+}
+.mek-button:hover
+{
+    background-color: rgba(0, 0, 0, 1);
+    box-shadow: inset 1px 1px 1px 1px #fff;
+    text-shadow: 1px 1px 1px #000, -1px 1px 1px #000, -1px -1px 1px #000, 1px -1px 1px #000;
+    /*background-image: linear-gradient(to left top, rgba(247,0,0,1) 0%, 
+    rgba(255, 129, 110,1) 49%, 
+    rgba(255, 129, 110,1) 51%, 
+    rgba(185,0,0,1) 100%)*/
+}
+@keyframes clicked
+{
+    0%
+    {
+        background-image: linear-gradient(70deg, 
+            rgb(255, 180, 0) 0%,
+            rgb(255, 255, 0) 49%,
+            rgb(255, 255, 0) 51%,
+            rgb(255, 160, 0) 100%);
+        color: #222 !important;
+    }
+    100%
+    {
+        background-color: rgba(0, 0, 0, 0.7);
+        color: #e2e2e2;
+    }
+}
+.mek-button-clicked
+{
+    padding: 5px 15px 5px 15px;
+    margin: 5px;
+    cursor: pointer;
+    animation: 500ms clicked;
+    border-radius: 30px;
+    border: 1px solid #222;
+    box-shadow: inset 0px 0px 1px 1px #222;
+}
 </style>
