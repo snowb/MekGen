73d75034a1e26fecf6187ff1ae3e6bda600e5e6f
snowb <ammoset@gmail.com>
2019-04-16 18:44:59 +0200

    creation of cleanComponent module

---
+++ src/store/modules/clean_component_module.js
@@ -0,0 +1,38 @@
+let cleanComponent=function(_component)
+{
+    if(Array.isArray(_component))
+    {
+        _component.forEach((_element)=>
+        {
+            if(Array.isArray(_element) || typeof _element==="object")
+            {
+                _element=this.cleanComponent(_element);
+            }
+            else if(!/^[\w\d-_.() ]+$/.test(_element) || _element==="")
+            {
+                _element=null;
+            }
+        });
+    }
+    else if(typeof _component==="object")
+    {
+        for(let prop in _component)
+        {
+            if(Array.isArray(_component[prop]) || typeof _component[prop]==="object")
+            {
+                _component[prop]=this.cleanComponent(_component[prop]);
+            }
+            else if(!/^[\w\d-_.() ]+$/.test(_component[prop]) || _component[prop]==="")
+            {
+                _component[prop]=null;
+            }
+        }
+    }
+    else if(!/^[\w\d-_.() ]+$/.test(_component) || _component==="")
+    {
+        _component=null;
+    }
+    return _component;
+};
+
+export {cleanComponent};
