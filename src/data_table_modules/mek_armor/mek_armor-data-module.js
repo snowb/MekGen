032f5f35dfd31ee0f5cc9c79167578d5e9b5eaad
snowb <ammoset@gmail.com>
2019-04-16 19:03:09 +0200

    switch to in-module table update

--- src/data_table_modules/mek_armor/mek_armor-data-module.js
+++ src/data_table_modules/mek_armor/mek_armor-data-module.js
@@ -1,69 +1,56 @@
 //data table module, raw data output for re-use in non-vue-component formats
 import servo_classes_data from "@/data_table_modules/universal/servo_classes_data.js";
-import {partial_validate, partial_has_feature, partial_get_feature} from "@/data_table_modules/universal/mek_partial-function-data-module";
+import {partial_validate, partial_has_feature, partial_get_feature, partial_cleaned_feature} from "@/data_table_modules/universal/mek_partial-function-data-module";
 
 //create new armor_table based on servo_classes_data
 let default_data={name:"None",cost:0,stopping_power:0,code:0};
-let armor_table=[default_data];
+let raw_armor_table=[default_data];
 
 let new_armor_table=servo_classes_data.map((_val)=>
 {
     return {name: _val.name, cost: _val.code, stopping_power: _val.code, code:_val.code};
 });
-let armor_data_table=armor_table.concat(new_armor_table);
+raw_armor_table=raw_armor_table.concat(new_armor_table);
 let data_table_keys=["name","cost","stopping_power","code"];
+let data_cached=false;
+let armor_data_table=null;
 
-//data validator for armor_data_table
-//call partial_validate with appropriate data for full validate
-let armor_validate=partial_validate(armor_data_table, data_table_keys);
-
-//completed function for checking if data has data
-let has_feature=partial_has_feature(armor_data_table);
-
-//completed function for returning matching data
-let get_feature=partial_get_feature(armor_data_table, has_feature);
-
-/*** 
- * 
- * determine if can be switched to some form of partial function format 
- * 
- * ****/
-let cleaned_feature=function(_pkey, _feature, _filtered_table)
-{//input: primary key, selected feature, filtered data table
-    let data=undefined;
-    let key_list=[];
-    let update=false;
-    let alerts=[];
-    let json_data=JSON.stringify(_feature);
-    let filtered_table=_filtered_table===undefined?armor_data_table:_filtered_table;
-    let has_filtered_feature=filtered_table.some((_val)=>
+let filter_data_table=(_servo_class)=>
+{
+    data_cached=false;
+    armor_data_table=raw_armor_table.filter((_val)=>
     {
-        return _val[_pkey]==_feature[_pkey];
+        return _val.code<=_servo_class;
     });
+};
 
-    if(_feature===undefined || !has_feature(_pkey,_feature[_pkey]) || !has_filtered_feature)
+let cached_validate=partial_validate(raw_armor_table, data_table_keys);
+//data validator for feature_data_table
+//call partial_validate with appropriate data for full validate
+let armor_validate=(_data)=>
+{
+    if(data_cached)
     {
-        data=default_data;
-        key_list=[data[_pkey]];
-        update=true;
-        alerts.push("Mek_Armor: "+json_data);
-        alerts.push("**** Invalid data. Reseting to default. ****");
+        return cached_validate(_data);
     }
-    else if(!armor_validate(_feature))
-    {
-        data=JSON.parse(JSON.stringify(get_feature(_pkey,_feature[_pkey])));
-        key_list=[data[_pkey]];
-        update=true;
-        alerts.push("Mek_Armor: "+json_data);
-        alerts.push("**** Invalid data. Reseting. ****");
-    }
-    else
-    {
-        data=JSON.parse(JSON.stringify(_feature));
-        key_list=[data[_pkey]];
-        update=false;
-    }
-    return {data:data, key_list:key_list, update:update, alerts:alerts};
+    data_cached=true;
+    cached_validate=partial_validate(armor_data_table, data_table_keys);
+    return cached_validate(_data);
 }
 
-export {armor_data_table, armor_validate, has_feature, get_feature, cleaned_feature};
+//completed function for checking if data has data
+let has_feature=(_pkey,_data)=>
+{
+    return partial_has_feature(armor_data_table)(_pkey,_data);
+}
+
+//completed function for returning matching data
+let get_feature=(_pkey,_data)=>
+{
+    return partial_get_feature(armor_data_table, has_feature)(_pkey,_data);
+}
+
+let cleaned_feature=
+    partial_cleaned_feature(armor_validate, has_feature, get_feature, default_data, "Mek_Armor");
+
+export {armor_data_table, armor_validate, has_feature, get_feature, cleaned_feature, filter_data_table};
