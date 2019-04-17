d2b77c8eebf9599df6673d2c0153ec47c73d8756
snowb <ammoset@gmail.com>
2019-04-17 19:02:11 +0200

    added spacing

--- src/store/modules/mek_armor_validators.js
+++ src/store/modules/mek_armor_validators.js
@@ -6,11 +6,13 @@ import(/* webpackChunkName: "mek_armor-data-module" */"@/data_table_modules/mek_
     mek_armor_validators.mek_armor=_module.cleaned_feature;
     mek_armor_validators.mek_armor_filter=_module.filter_data_table;
 });
+
 import(/* webpackChunkName: "mek_armor-type-data-module" */"@/data_table_modules/mek_armor/mek_armor-type-data-module.js")
 .then((_module)=>
 {
     mek_armor_validators.mek_armor_type=_module.cleaned_feature;
 });
+
 import(/* webpackChunkName: "mek_armor-RAM-data-module" */"@/data_table_modules/mek_armor/mek_armor-RAM-data-module.js")
 .then((_module)=>
 {
