49f6d525506f3744bc47aba63c522a42e94681f6
snowb <ammoset@gmail.com>
2019-04-17 20:14:37 +0200

    add scoped classes for proper display

--- src/design_components/mek_melee/subcomponents/mek_melee-entangle-range.vue
+++ src/design_components/mek_melee/subcomponents/mek_melee-entangle-range.vue
@@ -1,8 +1,8 @@
 <template>
     <span class="mek-flex-col">
-        <div class="metallic_background_small">
-            <div class="subsection_container" @mousedown="$event.preventDefault()">
-                <div class="subsection_header_small">Entangle&nbsp;Range</div>
+        <div class="metallic_background_mer">
+            <div class="subsection_container_mer" @mousedown="$event.preventDefault()">
+                <div class="subsection_header_mer">Entangle&nbsp;Range</div>
                 <div>
                     <span @click="incrementProperty('damage')" style="vertical-align:middle;">
                         <v-icon name="plus-square"></v-icon>
@@ -115,5 +115,45 @@ export default
     font-size:120%;
     color:#222;
 }
-
+.metallic_background_mer
+{
+    background-image: linear-gradient(to left top, rgba(247,0,0,1) 0%, 
+        rgba(255, 129, 110,1) 49%, 
+        rgba(255, 129, 110,1) 51%, 
+        rgba(185,0,0,1) 100%);
+    height: 100%;
+    width: 100%;
+    padding:5px;
+    border-radius: 7px;
+    box-shadow: #222 0px 0px 0px 2px, #fff 0px 0px 5px 2px;
+    padding-top:34px;
+}
+.subsection_container_mer
+{
+    position: relative;
+    border-radius: 7px;
+    padding: 10px;
+    background-color: rgb(170, 170, 170);
+    height: 100%;
+    width: 100%;
+    box-shadow: rgb(34, 34, 34) 0px 0px 0px 2px inset, rgb(255, 255, 255) 0px 0px 5px 2px inset;
+}
+.subsection_header_mer
+{
+    font-weight: bold;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%,0%);
+    top: -32px;
+    font-family: Arial Black, sans-serif;
+    font-size: 110%;
+    border: 1px solid black;
+    padding: 2px 10px;
+    border-radius: 7px;
+    box-shadow: rgba(255, 255, 255, 0.5) -1px -1px 1px 1px inset, rgba(0,0,0,0.5) 1px 1px 1px 3px inset;
+    color: #ddd;
+    text-shadow: 1px 1px 1px #000, 1px -1px 1px #000, -1px -1px 1px #000, -1px 1px 1px #000;
+    background-color: rgba(0,0,0,0.2);
+    white-space: nowrap;
+}
 </style>
