7db2be9a98bb1af2f1d03636c16d1dcfaf8d3a25
snowb <ammoset@gmail.com>
2019-04-21 12:33:10 +0200

    change to explicit chunk name

--- src/store/modules/validators/mek_servo_validators.js
+++ src/store/modules/validators/mek_servo_validators.js
@@ -1,19 +1,19 @@
 let validators={};
 
-import(/* webpackChunkName: "[request]" */"@/data_table_modules/mek_servo/mek_servo-class-data-module.js")
+import(/* webpackChunkName: "mek_servo-class-data-module" */"@/data_table_modules/mek_servo/mek_servo-class-data-module.js")
 .then((_module)=>
 {
     validators.servo=_module.cleaned_feature;
     validators.create_class_table=_module.create_class_data_table;
 });
 
-import(/* webpackChunkName: "[request]" */"@/data_table_modules/mek_servo/mek_servo-kills-space-trade-data-module.js")
+import(/* webpackChunkName: "mek_servo-kills-space-trade-data-module" */"@/data_table_modules/mek_servo/mek_servo-kills-space-trade-data-module.js")
 .then((_module)=>
 {
     validators.trade=_module.cleaned_feature;
 });
 
-import(/* webpackChunkName: "[request]" */"@/data_table_modules/mek_servo/mek_servo-type-data-module.js")
+import(/* webpackChunkName: "mek_servo-type-data-module" */"@/data_table_modules/mek_servo/mek_servo-type-data-module.js")
 .then((_module)=>
 {
     validators.type=_module.cleaned_feature;
