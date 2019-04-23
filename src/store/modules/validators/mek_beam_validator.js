53953ffc013f325897ef91450ce9b3b6b2b189fc
snowb <ammoset@gmail.com>
2019-04-23 12:34:56 +0200

    moved beam validator to moduke

--- src/store/modules/validators/mek_beam_validator.js
+++ src/store/modules/validators/mek_beam_validator.js
@@ -1,4 +1,5 @@
 let validators={};
+let alerts=[];
 
 import(/* webpackChunkName: "mek_beam-accuracy-data-module" */"@/data_table_modules/mek_beam/mek_beam-accuracy-data-module")
 .then((_module)=>
@@ -51,4 +52,67 @@ import(/* webpackChunkName: "mek_beam-wide-angle-data-module" */"@/data_table_mo
     validators.wide_angle=_module.cleaned_feature;
 });
 
-export {validators};
+let runValidator=(_validator, _pkey, _component)=>
+{
+    return _validator(_pkey,_component);
+};
+
+let validateComponent=(_component)=>
+{
+    let cleanedComponent=_component;
+    let validatedData;
+    /**
+     * 
+     * implement array-of-objects componentsToValidate
+     * implement runValidator forEach loop of above
+     * 
+     */
+    let componentsToValidate=
+    [
+        {validator:validators.damage,pkey:"damage",component_prop:"selected_damage"},
+        {validator:validators.burst_value,pkey:"burst_value",component_prop:"selected_burst_value"},
+        {validator:validators.accuracy,pkey:"accuracy",component_prop:'selected_accuracy'},
+        {validator:validators.warm_up,pkey:"time",component_prop:'selected_warm_up_time'},
+        {validator:validators.wide_angle,pkey:"angle",component_prop:'selected_wide_angle'},
+    ];
+    componentsToValidate.forEach((_val)=>
+    {//loop thru and validate mek_beam damage, burst_val, accuracy, warm_up, wide_angle
+        validatedData=runValidator(_val.validator,_val.pkey,_component[_val.component_prop]);
+        cleanedComponent[_val.component_prop]=validatedData.data;
+        alerts=alerts.concat(validatedData.alerts);
+    });
+
+    //extract base range for range_mod update
+    let base_range=cleanedComponent.selected_damage.range;
+    //update range_mod table
+    validators.update_range_mod(base_range);
+    //validate range mod
+    validatedData=validators.range_mod("range_mod",_component.selected_range_mod);
+    if(validatedData.update){alerts=alerts.concat(validatedData.alerts);}
+    cleanedComponent.selected_range_mod=validatedData.data;
+    //extract BV for feature data table update
+    let burst_value=cleanedComponent.selected_burst_value.burst_value;
+    //update feature table
+    validators.filter_feature(burst_value);
+    //validate features
+    validatedData=validators.feature(_component.feature_array,"feature");
+    if(validatedData.update){alerts=alerts.concat(validatedData.alerts);}
+    cleanedComponent.feature_array=validatedData.cleaned_array;
+    //extract if Mag-Fed
+    let magFed=cleanedComponent.feature_array.some(_val=>_val.feature=="Mag-Fed");
+    //update shots table
+    validators.update_shots(magFed);
+    //validate shots
+    validatedData=validators.shots("shots",_component.selected_shots);
+    if(validatedData.update){alerts=alerts.concat(validatedData.alerts);}
+    cleanedComponent.selected_shots=validatedData.data;
+
+    return cleanedComponent;
+};
+
+let getAlerts=()=>
+{
+    return alerts;
+};
+
+export {validateComponent,getAlerts};
--- src/store/modules/validators/mek_master_validator.js
+++ src/store/modules/validators/mek_master_validator.js
@@ -13,78 +13,21 @@ let importValidator=(_module_name, _validator_prop_name)=>
     import(/* webpackChunkName: "[request]" */`${_module_name}`)
     .then((_module)=>
     {
-        validators[_validator_prop_name]=_module.validators;
+        validators[_validator_prop_name]=_module;
     });
 };
 
 let module_list=
 [
-    {module_name:"./mek_armor_validator",validator_prop_name:"mek_armor"},
-    {module_name:"./mek_servo_validator",validator_prop_name:"mek_servo"},//import mek_servo component validator
-    {module_name:"./mek_beam_validator",validator_prop_name:"mek_beam"},
+    //mek_armor is sub-comp used in servo and shield
+    //{module_name:"./mek_armor_validator",validator_prop_name:"mek_armor"},
+    {module_name:"./mek_servo_validator",validator_prop_name:"mek_servo"},//import mek_servo validator
+    {module_name:"./mek_beam_validator",validator_prop_name:"mek_beam"},//import mek_beam validator
 ];
 module_list.forEach((_val)=>
 {
     importValidator(_val.module_name,_val.validator_prop_name);
 });
-
-let runValidator=(_validator, _pkey, _component)=>
-{
-    return _validator(_pkey,_component);
-};
-
-let validateBeam=(_component)=>
-{
-    let cleanedComponent=_component;
-    let validatedData;
-    /**
-     * 
-     * implement array-of-objects componentsToValidate
-     * implement runValidator forEach loop of above
-     * 
-     */
-    let componentsToValiate=
-    [
-        {validator:validators.mek_beam.damage,pkey:"damage",component_prop:"selected_damage"},
-        {validator:validators.mek_beam.burst_value,pkey:"burst_value",component_prop:"selected_burst_value"},
-        {validator:validators.mek_beam.accuracy,pkey:"accuracy",component_prop:'selected_accuracy'},
-        {validator:validators.mek_beam.warm_up,pkey:"time",component_prop:'selected_warm_up_time'},
-        {validator:validators.mek_beam.wide_angle,pkey:"angle",component_prop:'selected_wide_angle'},
-    ];
-    componentsToValiate.forEach((_val)=>
-    {//loop thru and validate mek_beam damage, burst_val, accuracy, warm_up, wide_angle
-        validatedData=runValidator(_val.validator,_val.pkey,_component[_val.component_prop]);
-        cleanedComponent[_val.component_prop]=validatedData.data;
-        alerts=alerts.concat(validatedData.alerts);
-    });
-
-    //extract base range for range_mod update
-    let base_range=cleanedComponent.selected_damage.range;
-    //update range_mod table
-    validators.mek_beam.update_range_mod(base_range);
-    //validate range mod
-    validatedData=validators.mek_beam.range_mod("range_mod",_component.selected_range_mod);
-    if(validatedData.update){alerts=alerts.concat(validatedData.alerts);}
-    cleanedComponent.selected_range_mod=validatedData.data;
-    //extract BV for feature data table update
-    let burst_value=cleanedComponent.selected_burst_value.burst_value;
-    //update feature table
-    validators.mek_beam.filter_feature(burst_value);
-    //validate features
-    validatedData=validators.mek_beam.feature(_component.feature_array,"feature");
-    if(validatedData.update){alerts=alerts.concat(validatedData.alerts);}
-    cleanedComponent.feature_array=validatedData.cleaned_array;
-    //extract if Mag-Fed
-    let magFed=cleanedComponent.feature_array.some(_val=>_val.feature=="Mag-Fed");
-    //update shots table
-    validators.mek_beam.update_shots(magFed);
-    //validate shots
-    validatedData=validators.mek_beam.shots("shots",_component.selected_shots);
-    if(validatedData.update){alerts=alerts.concat(validatedData.alerts);}
-    cleanedComponent.selected_shots=validatedData.data;
-
-    return cleanedComponent;
-};
 
 let validateComponent=(_component)=>
 {
@@ -114,15 +57,8 @@ let validateComponent=(_component)=>
                 alerts.concat(validators.mek_servo.getAlerts());
                 break;
             case "beam":
-                cleanedComponent=validateBeam(_component);
-                //damage
-                //accuracy
-                //brust val
-                //range mod
-                //warm up time
-                //shots (>0)
-                //wide angle
-                //features
+                cleanedComponent=validators.mek_beam.validateComponent(_component);
+                alerts.concat(validators.mek_beam.getAlerts());
                 //efficiency!?
                 break;
             case "emw":
