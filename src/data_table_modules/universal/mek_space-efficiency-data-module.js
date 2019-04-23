8240c71cac3771fa757a1226b45858f721d0b4f4
snowb <ammoset@gmail.com>
2019-04-23 20:38:18 +0200

    add more data checking

--- src/data_table_modules/universal/mek_space-efficiency-data-module.js
+++ src/data_table_modules/universal/mek_space-efficiency-data-module.js
@@ -1,6 +1,6 @@
 let isValid=(_val)=>
 {
-  return _val!==undefined && _val!==null && _val!=="";
+  return _val!==undefined && _val!==null && _val!="";
 };
 
 let validate_efficiency=(_efficiency, _raw_space, _parent_component_name)=>
@@ -8,7 +8,7 @@ let validate_efficiency=(_efficiency, _raw_space, _parent_component_name)=>
   let update=false;
   let data={modifier:0,cost:0};
   let alerts=[];
-  let parent_component_name=isValid(_parent_component_name) ? "Unknown" : _parent_component_name;
+  let parent_component_name=isValid(_parent_component_name) ? _parent_component_name : "Unknown";
 
   switch(true)
   {
@@ -34,14 +34,12 @@ let validate_efficiency=(_efficiency, _raw_space, _parent_component_name)=>
     alerts.push("**** Missing Modifier, reseting based on Cost.");
     update=true;
     data.modifier=data.cost*2;
-    data.cost=data.cost;
   }
   if(isNaN(data.cost))
   {
     alerts.push(parent_component_name+" - Mek_Space-Efficiency:");
     alerts.push("**** Missing Cost, reseting based on Modifier.");
     update=true;
-    data.modifier=data.modifier;
     data.cost=data.modifier/2;
   }
   if(data.modifier>_raw_space)
@@ -51,6 +49,13 @@ let validate_efficiency=(_efficiency, _raw_space, _parent_component_name)=>
     update=true;
     data.modifier=0;
     data.cost=0;
+  }
+  if(data.cost!=(data.modifier/2))
+  {
+    alerts.push(parent_component_name+" - Mek_Space-Efficiency:");
+    alerts.push("**** Modifier/Cost inconsistent. Reseting to Modifier.");
+    update=true;
+    data.cost=data.modifier/2;
   }
 
   return {update:update,data:data,alerts:alerts};
