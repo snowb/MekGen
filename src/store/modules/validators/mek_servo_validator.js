db7ff220806d05579bfcae372235b52de2bff6ed
snowb <ammoset@gmail.com>
2019-04-23 18:16:14 +0200

    import mek_armor_validator module

--- src/store/modules/validators/mek_servo_validator.js
+++ src/store/modules/validators/mek_servo_validator.js
@@ -20,23 +20,10 @@ import(/* webpackChunkName: "mek_servo-type-data-module" */"@/data_table_modules
     validators.servo_type=_module.cleaned_feature;
 });
 
-import(/* webpackChunkName: "mek_armor-data-module" */"@/data_table_modules/mek_armor/mek_armor-data-module.js")
+import(/* webpackChunkName: "mek_armor_validator" */"./mek_armor_validator")
 .then((_module)=>
 {
-    validators.armor=_module.cleaned_feature;
-    validators.armor_filter=_module.filter_data_table;
-});
-
-import(/* webpackChunkName: "mek_armor-type-data-module" */"@/data_table_modules/mek_armor/mek_armor-type-data-module.js")
-.then((_module)=>
-{
-    validators.armor_type=_module.cleaned_feature;
-});
-
-import(/* webpackChunkName: "mek_armor-RAM-data-module" */"@/data_table_modules/mek_armor/mek_armor-RAM-data-module.js")
-.then((_module)=>
-{
-    validators.armor_RAM=_module.cleaned_feature;
+    validators.armor=_module.validators;
 });
 
 let runValidator=(_validator, _pkey, _component)=>
@@ -51,8 +38,8 @@ let validateComponent=(_component)=>
     let componentsToValidate=
     [
         {validator:validators.servo_type,pkey:"type",component_prop:"selected_servo_type"},
-        {validator:validators.armor_type,pkey:"damage_coefficient",component_prop:"selected_armor_type"},
-        {validator:validators.armor_RAM,pkey:"absorption",component_prop:'selected_absorption'},
+        {validator:validators.armor.type,pkey:"damage_coefficient",component_prop:"selected_armor_type"},
+        {validator:validators.armor.RAM,pkey:"absorption",component_prop:'selected_absorption'},
     ];
     componentsToValidate.forEach((_val)=>
     {//loop thru and validate mek_servo-type, mek_armor-type, and mek_armor-RAM
@@ -81,9 +68,9 @@ let validateComponent=(_component)=>
     alerts=alerts.concat(validatedData.alerts);
     cleanedComponent.kills_space_trade=validatedData.data;
     //update armor based on servo class
-    validators.armor_filter(_component.selected_servo_class.code+2);
+    validators.armor.armor_filter(_component.selected_servo_class.code+2);
     //validate armor
-    validatedData=validators.armor("code",_component.selected_armor);
+    validatedData=validators.armor.armor("code",_component.selected_armor);
     alerts=alerts.concat(validatedData.alerts);
     cleanedComponent.selected_armor=validatedData.data;
 
