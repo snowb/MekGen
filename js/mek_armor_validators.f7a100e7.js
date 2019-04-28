1fcbe08c0d955dc10f936fdf98e3d6a4d7b530eb
snowb <ammoset@gmail.com>
2019-04-28 12:22:03 +0200

    add EMW to validator, other bug fixes

--- js/mek_armor_validators.f7a100e7.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["mek_armor_validators"],{db1c:function(e,a,t){"use strict";t.r(a),t.d(a,"validators",function(){return n});let n={};t.e("mek_armor-data-module").then(t.bind(null,"4753")).then(e=>{n.armor=e.cleaned_feature,n.armor_filter=e.filter_data_table}),t.e("mek_armor-type-data-module").then(t.bind(null,"e492")).then(e=>{n.type=e.cleaned_feature}),t.e("mek_armor-RAM-data-module").then(t.bind(null,"4277")).then(e=>{n.RAM=e.cleaned_feature})}}]);
-//# sourceMappingURL=mek_armor_validators.f7a100e7.js.map
