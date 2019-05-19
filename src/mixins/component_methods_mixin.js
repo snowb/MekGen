bf831332cb7c64f169fb3a33cad02ac94b9f1274
snowb <ammoset@gmail.com>
2019-05-19 12:21:58 +0200

    attempt to ensure unique data on ingest

--- src/mixins/component_methods_mixin.js
+++ src/mixins/component_methods_mixin.js
@@ -5,6 +5,7 @@ export default
         universal_ingest_data(_data_object,_alertMessage)
         {
             this.original_component=JSON.stringify(_data_object);//store a copy as a JSON object for 'reset' purposes
+            let data_object=JSON.parse(this.original_component);
             if(_data_object===null)
             {
                 this.componentSaveReset("clear");
@@ -12,30 +13,30 @@ export default
                 this.$store.commit("alertMessage",_alertMessage);
             }
 
-            for(let _property in _data_object)
+            for(let _property in data_object)
             {//exclude computed properties that are auto updated
                 if(Object.keys(this._computedWatchers).includes(_property))
                 {
                     continue;
                 }
-                if(typeof _data_object[_property]==="object" && !Array.isArray(_data_object[_property]))
+                if(typeof data_object[_property]==="object" && !Array.isArray(data_object[_property]))
                 {
                     if(typeof this[_property]!=="object" || this[_property]===null)
                     {
                         this[_property]={};
                     }
-                    for(let _sub_property in _data_object[_property])
+                    for(let _sub_property in data_object[_property])
                     {
-                        this.$set(this[_property],[_sub_property],_data_object[_property][_sub_property]);
+                        this.$set(this[_property],[_sub_property],data_object[_property][_sub_property]);
                     }
                 }
-                else if(Array.isArray(_data_object[_property]))
+                else if(Array.isArray(data_object[_property]))
                 {
-                    this.$set(this,_property,_data_object[_property]);
+                    this.$set(this,_property,data_object[_property]);
                 }
                 else
                 {
-                    this.$set(this,_property,_data_object[_property]);
+                    this.$set(this,_property,data_object[_property]);
                 }
             }
             this.$forceUpdate();
