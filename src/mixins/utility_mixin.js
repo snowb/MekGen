4b4ee9850e59099187c19cd1b640315ab65cbff1
snowb <ammoset@gmail.com>
2019-06-22 17:07:39 +0200

    add short_uuid function

--- src/mixins/utility_mixin.js
+++ src/mixins/utility_mixin.js
@@ -28,7 +28,12 @@ export default
             return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
                 (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
             )
-              
+        },
+        create_short_uuid:function()
+        {
+            return ([1e7]+-1e3).replace(/[018]/g, c =>
+                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+            );
         },
         invisible_pad:function(_array_of_items)
         {
